569b25310009012392ed73325706238db1bb8b13
Oleg.Korshul <Oleg.Korshul@OnlyOffice.com>
2013-06-04 09:56:11 +0000

    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@47028 954022d7-b5bf-4e40-9824-e11837661b57

--- Excel/menu/menu.js
+++ Excel/menu/menu.js
@@ -471,7 +471,7 @@
 		console.log("Cell Edit State - " + state);
 	});
 
-	api.asc_Init("../Word/FontsFreeType/FontFiles/");
+	api.asc_Init("../Fonts/");
 	//api.asc_setViewerMode(true);
 
 	function getURLParameter(name) {
