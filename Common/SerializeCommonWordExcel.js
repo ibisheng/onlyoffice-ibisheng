f797a2a2745d5be4dc9f57e5f2a61cc21f587c9e
Sergey.Konovalov <Sergey.Konovalov@OnlyOffice.com>
2013-06-19 16:11:06 +0000

    заглушка для посещенных гиперссылок
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@47351 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/SerializeCommonWordExcel.js
+++ Common/SerializeCommonWordExcel.js
@@ -152,6 +152,9 @@ function BinaryCommonWriter(memory)
     {
 		if(color instanceof ThemeColor)
 		{
+			//заглушка для посещенных гиперссылок
+			if(g_nColorHyperlinkVisited == color.theme && null == color.tint)
+				color.theme = g_nColorHyperlink;
 			if(null != color.theme)
 			{
 				this.memory.WriteByte(c_oSer_ColorObjectType.Theme);
