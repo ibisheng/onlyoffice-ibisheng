d95b837dabe5754468b97161019ce89d2cfb4593
Alexander.Trofimov <Alexander.Trofimov@OnlyOffice.com>
2013-06-24 14:44:08 +0000

    Добавил список языков для spellCheck
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@47431 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ Common/spellCheckLanguage.js
@@ -0,0 +1,58 @@
+﻿/* spellCheckLanguages.js
+ *
+ * Author: Alexander.Trofimov
+ * Date: 24.06.13
+ */
+(	/**
+ * @param {jQuery} $
+ * @param {Window} window
+ * @param {undefined} undefined
+ */
+	function ($, window, undefined) {
+		/*
+		 * Import
+		 * -----------------------------------------------------------------------------
+		 */
+		var asc = window["Asc"] ? window["Asc"] : (window["Asc"] = {});
+		var prot;
+
+		/**
+		 * Класс language для получения списка языков в проверке орфографии
+		 * -----------------------------------------------------------------------------
+		 * @constructor
+		 * @memberOf Asc
+		 * @param id
+		 * @param name
+		 * @return {*}
+		 */
+		function asc_CLanguage (name, id) {
+			if ( !(this instanceof asc_CLanguage) ) {
+				return new asc_CLanguage(name, id);
+			}
+
+			this.name	= name;			// имя языка
+			this.id		= id;			// уникальный id языка
+
+			return this;
+		}
+
+		asc_CLanguage.prototype = {
+			constructor: asc_CLanguage,
+			asc_getId: function () { return this.id; },
+			asc_getName: function () { return this.name; },
+			asc_setId: function (val) { this.id = val; },
+			asc_setName: function (val) { this.name = val; }
+		};
+
+		/*
+		 * Export
+		 * -----------------------------------------------------------------------------
+		 */
+		window["Asc"]["asc_CLanguage"] = window["Asc"].asc_CLanguage = asc_CLanguage;
+		prot = asc_CLanguage.prototype;
+		prot["asc_getId"]			= prot.asc_getId;
+		prot["asc_getName"]			= prot.asc_getName;
+		prot["asc_setId"]			= prot.asc_setId;
+		prot["asc_setName"]			= prot.asc_setName;
+	}
+)(jQuery, window);
