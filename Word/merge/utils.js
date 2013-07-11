4538101532cd8bf38efc38552645d9a4f1cdfba4
Alexey.Golubev <Alexey.Golubev@OnlyOffice.com>
2013-07-11 13:43:26 +0000

    Настройка среды для nodejs.
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@47898 954022d7-b5bf-4e40-9824-e11837661b57

--- Word/merge/utils.js
+++ Word/merge/utils.js
@@ -1,10 +1,18 @@
-var Canvas = require('canvas');
-var Image = Canvas.Image;
-var jsdom = require("jsdom").jsdom;
-var document = jsdom(null, null);
-var window = document.createWindow();
-var navigator = require('navigator');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var $ = require('jQuery');
-var jq = require('jQuery').create();
-var jQuery = require('jQuery').create(window);
+if (typeof window === 'undefined') {
+	//Run under nodejs
+	var Canvas = require('canvas');
+	var Image = Canvas.Image;
+	var jsdom = require('jsdom')['jsdom'];
+	var document = jsdom(null, null);
+	var window = document.createWindow();
+	var navigator = require('navigator');
+	var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+	var $ = require('jQuery');
+	var jq = require('jQuery').create();
+	var jQuery = require('jQuery').create(window);
+}
+else {
+	//Run under browser
+	window.exports = {};
+}
+
