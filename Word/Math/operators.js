4420c7b969914887c4ac9ade1b1695fef1689343
Anna.Pavlova <Anna.Pavlova@OnlyOffice.com>
2013-09-12 15:32:54 +0000

    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@49728 954022d7-b5bf-4e40-9824-e11837661b57

--- Word/Math/operators.js
+++ Word/Math/operators.js
@@ -203,7 +203,7 @@ CGlyphOperator.prototype.draw_other = function() //  с выравнивание
     MathControl.pGraph.SetIntegerGrid(intGrid);
 
 }
-CGlyphOperator.prototype.getCoordinateGlyph = function()
+CGlyphOperator.prototype.new_getCoordinateGlyph = function()
 {
     var coord = this.calcCoord(this.measure);
 
@@ -266,6 +266,89 @@ CGlyphOperator.prototype.getCoordinateGlyph = function()
     {
         a1 *= -1; b1 *= -1; c1 = glW;
         a2 *= -1; b2 *= -1; c2 = H;
+    }
+
+    var gpX = 0,
+        gpY = 0;
+
+    if(this.loc == 3)
+        gpX = - this.penW*25.4/96;
+
+    var XX = new Array(),
+        YY = new Array();
+
+    for(var i = 0; i < X.length; i++)
+    {
+        XX[i] = X[i]*a1 + Y[i]*b1 + c1 + gpX;
+        YY[i] = X[i]*a2 + Y[i]*b2 + c2 + gpY;
+    }
+
+    return {XX: XX, YY: YY, Width: glW, Height: glH};
+}
+CGlyphOperator.prototype.getCoordinateGlyph = function()
+{
+    var coord = this.calcCoord(this.measure);
+
+    var X = coord.XX, Y = coord.YY,
+        W =  this.size.width, H =  this.size.height;
+
+    var bHor = this.loc == 0 || this.loc  == 1;
+
+    var glW = 0, glH = 0;
+
+    if(bHor)
+    {
+        glW = coord.W;
+        glH = coord.H;
+    }
+    else
+    {
+        glW = coord.H;
+        glH = coord.W;
+    }
+
+    var shW =  (W - glW)/ 2, // выравниваем глиф по длине
+        shH =  (H - glH)/2; // при повороте на 90 градусовы
+
+    // A*x + B*y + C = 0
+
+    if(bHor)
+    {
+        a1 = 1; b1 = 0; c1 = 0;
+        a2 = 0; b2 = 1; c2 = shH;
+    }
+    else
+    {
+        a1 = 0; b1 = 1; c1 = shW;
+        a2 = 1; b2 = 0; c2 = 0;
+    }
+
+    /*var shW = 0,
+     shH = 0;
+
+     if(bHor)
+     {
+     a1 = 1; b1 = 0; c1 = 0;
+     a2 = 0; b2 = 1; c2 = 0;
+     }
+     else
+     {
+     a1 = 0; b1 = 1; c1 = 0;
+     a2 = 1; b2 = 0; c2 = 0;
+     }*/
+
+    if(this.turn == 1)
+    {
+        a1 *= -1; b1 *= -1; c1 = W - c1; //c1 = W - c1;
+    }
+    else if(this.turn == 2)
+    {
+        a2 *= -1; b2 *= -1; c2 = H - c2;
+    }
+    else if(this.turn == 3)
+    {
+        a1 *= -1; b1 *= -1; c1 = W - c1;
+        a2 *= -1; b2 *= -1; c2 = H - c2;
     }
 
     var gpX = 0,
@@ -1261,11 +1344,7 @@ function GetGlyph_GrChr(chr, location)
         operator.init(props);
     }
 
-
-
-
     return operator;
-
 }
 
 function CBaseDelimiter()
@@ -4350,8 +4429,8 @@ COperator.prototype.fixSize = function(measure)
         }
         else
         {
-            height = this.glyph.size.height;
-            width = measure > this.glyph.size.width ? measure : this.glyph.size.width; //this.glyph.size.width;
+            height = measure > this.glyph.size.height ? measure : this.glyph.size.height;
+            width = this.glyph.size.width;
         }
 
         var betta = this.TxtPrp.FontSize/36;
