4389cd7a8b2c7a22f7fcb294c1e10fcee2af94d8
Sergey.Luzyanin <Sergey.Luzyanin@OnlyOffice.com>
2013-10-03 09:43:32 +0000

    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@50387 954022d7-b5bf-4e40-9824-e11837661b57

--- Word/Editor/GraphicObjects/ObjectTypes/WordShape.js
+++ Word/Editor/GraphicObjects/ObjectTypes/WordShape.js
@@ -1025,20 +1025,25 @@ WordShape.prototype =
         var _l, _t, _r, _b;
 
         var _t_x_lt, _t_y_lt, _t_x_rt, _t_y_rt, _t_x_lb, _t_y_lb, _t_x_rb, _t_y_rb;
+        var body_pr = this.bodyPr;
+        var l_ins = typeof body_pr.lIns === "number" ? body_pr.lIns : 2.54;
+        var t_ins = typeof body_pr.tIns === "number" ? body_pr.tIns : 1.27;
+        var r_ins = typeof body_pr.rIns === "number" ? body_pr.rIns : 2.54;
+        var b_ins = typeof body_pr.bIns === "number" ? body_pr.bIns : 1.27;
         if(isRealObject(this.spPr.geometry) && isRealObject(this.spPr.geometry.rect))
         {
             var _rect = this.spPr.geometry.rect;
-            _l = _rect.l + _body_pr.lIns;
-            _t = _rect.t + _body_pr.tIns;
-            _r = _rect.r - _body_pr.rIns;
-            _b = _rect.b - _body_pr.bIns;
+            _l = _rect.l + l_ins;
+            _t = _rect.t + t_ins;
+            _r = _rect.r - r_ins;
+            _b = _rect.b - b_ins;
         }
         else
         {
-            _l = _body_pr.lIns;
-            _t = _body_pr.tIns;
-            _r = this.absExtX - _body_pr.rIns;
-            _b = this.absExtY - _body_pr.bIns;
+            _l = l_ins;
+            _t = t_ins;
+            _r = this.absExtX - r_ins;
+            _b = this.absExtY - b_ins;
         }
 
         if(_l >= _r)
@@ -1120,7 +1125,7 @@ WordShape.prototype =
                             _vertical_shift = (_text_rect_height - _content_height)*0.5;
                             break;
                         }
-                        case 4 ://t
+                        default ://t
                         {//Top
                             _vertical_shift = 0;
                             break;
@@ -1182,7 +1187,7 @@ WordShape.prototype =
                             _vertical_shift = (_text_rect_width - _content_height)*0.5;
                             break;
                         }
-                        case 4 ://t
+                        default ://t
                         {//Top
                             _vertical_shift = 0;
                             break;
@@ -1307,7 +1312,7 @@ WordShape.prototype =
                         _vertical_shift = (content_height2 - _content_height)*0.5;
                         break;
                     }
-                    case 4 ://t
+                    default ://t
                     {//Top
                         _vertical_shift = 0;
                         break;
