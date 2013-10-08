9bfac98b87cb82430f34cb1777db96f20b882acb
Dmitry.Shahtanov <Dmitry.Shahtanov@OnlyOffice.com>
2013-10-08 07:53:37 +0000

    частично исправлен Bug 21157 - Ошибка при вычислении функции SUMIF, если в диапазоне поиска/критерия содержатся пустые текстовые данные
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@50559 954022d7-b5bf-4e40-9824-e11837661b57

--- Excel/model/FormulaObjects/mathematicFunctions.js
+++ Excel/model/FormulaObjects/mathematicFunctions.js
@@ -3100,17 +3100,17 @@ cFormulaFunction.Mathematic = {
             }
             valueForSearching = parseNum( search ) ? new cNumber( search ) : new cString( search );
             if ( arg0 instanceof cArea ) {
-                val = arg0.getValue();
-                for ( var i = 0; i < val.length; i++ ) {
-                    if ( matching( val[i], valueForSearching, oper ) ) {
-                        var r = arg0.getRange(), ws = arg0.getWS(),
-                            r1 = r.first.getRow0() + i, c1 = arg2.getRange().first.getCol0();
-                        r = new cRef( ws.getRange3( r1, c1, r1, c1 ).getName(), ws );
-                        if ( r.getValue() instanceof cNumber ) {
-                            _sum += r.getValue().getValue();
+                var r = arg0.getRange().first.getRow0(), ws = arg0.getWS(), c1 = arg2.getRange().first.getCol0(), i = 0;
+                arg0.foreach2(function(c){
+                    if ( matching( c, valueForSearching, oper ) ) {
+                        var r1 = r + i,
+                        r2 = new cRef( ws.getRange3( r1, c1, r1, c1 ).getName(), ws );
+                        if ( r2.getValue() instanceof cNumber ) {
+                            _sum += r2.getValue().getValue();
                         }
                     }
-                }
+                    i++;
+                })
             }
             else {
                 val = arg0.getValue();
