b027a3e85d3eb08d5c7e990b598ea3b476f3696f
Alexey.Golubev <Alexey.Golubev@OnlyOffice.com>
2013-10-15 07:09:52 +0000

    добавлены версии инсталируемых модулей для nodejs
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@50845 954022d7-b5bf-4e40-9824-e11837661b57

--- Build/installtools.bat
+++ Build/installtools.bat
@@ -1,8 +1,8 @@
 @echo off
 
 echo Installation grunt and grunt-contrib
-call npm install -g grunt 
-call npm install grunt-contrib
-call npm install grunt-closure-tools
-call npm install grunt-exec
-call npm install grunt-replace
+call npm install -g grunt@0.3.17 
+call npm install grunt-contrib@0.3.0
+call npm install grunt-closure-tools@0.6.12
+call npm install grunt-exec@0.3.0
+call npm install grunt-replace@0.3.2
