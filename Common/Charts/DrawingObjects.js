77d1772048552153ccb3186405824d6976c540cc
Sergey.Luzyanin <Sergey.Luzyanin@OnlyOffice.com>
2013-10-25 11:46:00 +0000

    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@51294 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/Charts/DrawingObjects.js
+++ Common/Charts/DrawingObjects.js
@@ -4924,6 +4924,7 @@ function DrawingObjects() {
 			objectInfo.id = graphicObjectInfo.objectId;
 			objectInfo.object = _this.getDrawingBase(graphicObjectInfo.objectId);
 			objectInfo.cursor = graphicObjectInfo.cursorType;
+            objectInfo.hyperlink = graphicObjectInfo.hyperlink;
 			objectInfo.isGraphicObject = true;
 			
 			return objectInfo;
