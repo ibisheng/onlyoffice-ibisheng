ca036fef286884f1abd460187b61b623391d043c
Sergey.Luzyanin <Sergey.Luzyanin@OnlyOffice.com>
2013-11-05 12:16:38 +0000

    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@51729 954022d7-b5bf-4e40-9824-e11837661b57

--- Excel/model/History.js
+++ Excel/model/History.js
@@ -518,8 +518,8 @@ CHistory.prototype =
 		}
 		if(false == Point.bNoSelect && null != oSelectRange)
 			this.workbook.handlers.trigger("setSelection", oSelectRange.clone());
-		if (Point.SelectionState != null)
-			this.workbook.handlers.trigger("setSelectionState", Point.SelectionState);
+		//if (Point.SelectionState != null)
+		//	this.workbook.handlers.trigger("setSelectionState", Point.SelectionState);
 		
 		this._sendCanUndoRedo();
 
