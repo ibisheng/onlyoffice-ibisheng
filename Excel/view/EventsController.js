f9967075a970d3c0acae3ee5202f0a73b3c92f30
Dmitry.Vikulov <Dmitry.Vikulov@OnlyOffice.com>
2013-11-14 13:10:38 +0000

    Не увеличивалась ширина колонки при попадании в граф.объект
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@52063 954022d7-b5bf-4e40-9824-e11837661b57

--- Excel/view/EventsController.js
+++ Excel/view/EventsController.js
@@ -1316,7 +1316,7 @@
 				if ( graphicsInfo && graphicsInfo.isGraphicObject )
 					this.clickCounter.mouseMoveEvent(coord.x, coord.y);
 					
-				if ( asc["editor"].isStartAddShape || ( !t.isSelectMode && graphicsInfo) ) {
+				if ( asc["editor"].isStartAddShape || ( !t.isSelectMode && !t.isResizeMode && graphicsInfo) ) {
 					t.handlers.trigger("graphicObjectMouseMove", event, coord.x, coord.y);
 					t.handlers.trigger("updateWorksheet", t.element[0], coord.x, coord.y, event.ctrlKey, function(info){t.targetInfo = info;});
 					return;
