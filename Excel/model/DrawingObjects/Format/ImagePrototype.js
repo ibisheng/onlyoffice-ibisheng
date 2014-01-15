05f04c79882db8f423f5387374a6aa51ab9faa48
Sergey.Luzyanin <Sergey.Luzyanin@OnlyOffice.com>
2014-01-15 09:39:25 +0000

    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@53590 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ Excel/model/DrawingObjects/Format/ImagePrototype.js
@@ -0,0 +1,154 @@
+CImageShape.prototype.addToDrawingObjects =  CShape.prototype.addToDrawingObjects;
+CImageShape.prototype.setDrawingObjects = CShape.prototype.setDrawingObjects;
+CImageShape.prototype.setDrawingBase = CShape.prototype.setDrawingBase;
+CImageShape.prototype.deleteDrawingBase = CShape.prototype.deleteDrawingBase;
+CImageShape.prototype.addToRecalculate = CShape.prototype.addToRecalculate;
+CImageShape.prototype.convertPixToMM = CShape.prototype.convertPixToMM;
+CImageShape.prototype.getCanvasContext = CShape.prototype.getCanvasContext;
+CImageShape.prototype.getHierarchy = CShape.prototype.getHierarchy;
+CImageShape.prototype.getParentObjects = CShape.prototype.getParentObjects;
+CImageShape.prototype.recalculateTransform = CShape.prototype.recalculateTransform;
+CImageShape.prototype.recalculateBounds = CShape.prototype.recalculateBounds;
+CImageShape.prototype.deselect = CShape.prototype.deselect;
+CImageShape.prototype.hitToHandles = CShape.prototype.hitToHandles;
+CImageShape.prototype.hitInBoundingRect = CShape.prototype.hitInBoundingRect;
+CImageShape.prototype.getRotateAngle = CShape.prototype.getRotateAngle;
+
+CImageShape.prototype.setRecalculateInfo = function()
+{
+    this.recalcInfo =
+    {
+        recalculateBrush:          true,
+        recalculatePen:            true,
+        recalculateTransform:      true,
+        recalculateBounds:         true,
+        recalculateGeometry:       true,
+        recalculateStyle:          true,
+        recalculateFill:           true,
+        recalculateLine:           true,
+        recalculateTransparent:    true
+    };
+    this.bounds = {l: 0, t: 0, r: 0, b:0, w: 0, h:0};
+};
+
+CImageShape.prototype.recalcBrush = function()
+{
+    this.recalcInfo.recalculateBrush = true;
+};
+
+CImageShape.prototype.recalcPen = function()
+{
+    this.recalcInfo.recalculatePen = true;
+};
+CImageShape.prototype.recalcTransform = function()
+{
+    this.recalcInfo.recalculateTransform = true;
+};
+CImageShape.prototype.recalcBounds = function()
+{
+    this.recalcInfo.recalculateBounds = true;
+};
+CImageShape.prototype.recalcGeometry = function()
+{
+    this.recalcInfo.recalculateGeometry = true;
+};
+CImageShape.prototype.recalcStyle = function()
+{
+    this.recalcInfo.recalculateStyle = true;
+};
+CImageShape.prototype.recalcFill = function()
+{
+    this.recalcInfo.recalculateFill = true;
+};
+CImageShape.prototype.recalcLine = function()
+{
+    this.recalcInfo.recalculateLine = true;
+};
+CImageShape.prototype.recalcTransparent = function()
+{
+    this.recalcInfo.recalculateTransparent = true;
+};
+CImageShape.prototype.addToRecalculate = function()
+{
+    if(this.drawingObjects && this.drawingObjects.controller)
+    {
+        this.drawingObjects.controller.objectsForRecalculate[this.Id] = this;
+    }
+};
+CImageShape.prototype.handleUpdatePosition = function()
+{
+    this.recalcTransform();
+    this.addToRecalculate();
+};
+CImageShape.prototype.handleUpdateExtents = function()
+{
+    this.recalcGeometry();
+    this.addToRecalculate();
+};
+CImageShape.prototype.handleUpdateRot = function()
+{
+    this.recalcTransform();
+    this.recalcBounds();
+    this.addToRecalculate();
+};
+CImageShape.prototype.handleUpdateFlip = function()
+{
+    this.recalcTransform();
+    this.addToRecalculate();
+};
+CImageShape.prototype.handleUpdateFill = function()
+{
+    this.recalcBrush();
+    this.addToRecalculate();
+};
+CImageShape.prototype.handleUpdateLn = function()
+{
+    this.recalcLine();
+    this.addToRecalculate();
+};
+CImageShape.prototype.handleUpdateGeometry = function()
+{
+    this.recalcGeometry();
+    this.addToRecalculate();
+};
+CImageShape.prototype.convertPixToMM = CShape.prototype.convertPixToMM;
+CImageShape.prototype.getCanvasContext = CShape.prototype.getCanvasContext;
+CImageShape.prototype.getCompiledStyle = CShape.prototype.getCompiledStyle;
+CImageShape.prototype.getHierarchy = CShape.prototype.getHierarchy;
+CImageShape.prototype.getParentObjects = CShape.prototype.getParentObjects;
+CImageShape.prototype.recalculate = function () 
+{
+    if (this.recalcInfo.recalculateBrush) {
+        this.recalculateBrush();
+        this.recalcInfo.recalculateBrush = false;
+    }
+
+    if (this.recalcInfo.recalculatePen) {
+        this.recalculatePen();
+        this.recalcInfo.recalculatePen = false;
+    }
+    if (this.recalcInfo.recalculateTransform) {
+        this.recalculateTransform();
+        this.recalcInfo.recalculateTransform = false;
+    }
+
+    if (this.recalcInfo.recalculateGeometry) {
+        this.recalculateGeometry();
+        this.recalcInfo.recalculateGeometry = false;
+    }
+    if(this.recalcInfo.recalculateBounds)
+    {
+        this.recalculateBounds();
+        this.recalcInfo.recalculateBounds = false;
+    }
+};
+CImageShape.prototype.recalculateBounds = CShape.prototype.recalculateBounds;
+CImageShape.prototype.hitInInnerArea = CShape.prototype.hitInInnerArea;
+CImageShape.prototype.hitInPath = CShape.prototype.hitInPath;
+CImageShape.prototype.hitToHandles = CShape.prototype.hitToHandles;
+CImageShape.prototype.hitInBoundingRect = CShape.prototype.hitInBoundingRect;
+CImageShape.prototype.getNumByCardDirection = CShape.prototype.getNumByCardDirection;
+CImageShape.prototype.getCardDirectionByNum = CShape.prototype.getCardDirectionByNum;
+CImageShape.prototype.getResizeCoefficients = CShape.prototype.getResizeCoefficients;
+CImageShape.prototype.check_bounds = CShape.prototype.check_bounds;
+CImageShape.prototype.normalize = CShape.prototype.normalize;
