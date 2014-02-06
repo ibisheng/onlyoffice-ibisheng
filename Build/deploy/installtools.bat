2482711abb52c61c10e3749a56462706fc262799
Alexey.Golubev <Alexey.Golubev@OnlyOffice.com>
2014-02-06 11:38:14 +0000

    Устанавливаем модули для сборки только когда их действительно нет.
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@54203 954022d7-b5bf-4e40-9824-e11837661b57

--- Build/deploy/installtools.bat
+++ Build/deploy/installtools.bat
@@ -1,8 +1,15 @@
 @echo off
 cd /D %~dp0
+
+SET GRUNT=grunt@0.3.17
+SET GRUNT_CONTRIB=grunt-contrib@0.3.0
+SET GRUNT_CLOSURE_TOOLS=grunt-closure-tools@0.6.12
+SET GRUNT_EXEC=grunt-exec@0.3.0
+SET GRUNT_REPLACE=grunt-replace@0.3.2
+
 echo Installation grunt and grunt-contrib
-call npm install -g grunt@0.3.17 
-call npm install grunt-contrib@0.3.0
-call npm install grunt-closure-tools@0.6.12
-call npm install grunt-exec@0.3.0
-call npm install grunt-replace@0.3.2
+call npm list -g %GRUNT%			|| call npm install -g %GRUNT%
+call npm list %GRUNT_CONTRIB%		|| call npm install %GRUNT_CONTRIB%
+call npm list %GRUNT_CLOSURE_TOOLS%	|| call npm install %GRUNT_CLOSURE_TOOLS%
+call npm list %GRUNT_EXEC%			|| call npm install %GRUNT_EXEC%
+call npm list %GRUNT_REPLACE%		|| call npm install %GRUNT_REPLACE%
