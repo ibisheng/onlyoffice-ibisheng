c5869af3903cde42dbe515f63148c6fd5f7648a8
Oleg.Korshul <Oleg.Korshul@OnlyOffice.com>
2014-02-24 14:26:28 +0000

    деление на ноль
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@54610 954022d7-b5bf-4e40-9824-e11837661b57

--- Word/Drawing/Hit.js
+++ Word/Drawing/Hit.js
@@ -1,8 +1,13 @@
+var double_eps = 0.00001;
+var hit_error_eps = 0.5;
 function HitInLine(context, px, py, x0, y0, x1, y1)
 {
     var tx, ty, dx, dy, d;
     tx=x1-x0;
     ty=y1-y0;
+
+    if (Math.abs(tx*tx+ty*ty) < double_eps)
+        return HitInLine(context, px, py, x0, y0, x1 + hit_error_eps, y1 + hit_error_eps);
 
     d=1.5/Math.sqrt(tx*tx+ty*ty);
     if(global_mouseEvent !== null && typeof global_mouseEvent === "object" && typeof global_mouseEvent.KoefPixToMM === "number" && !isNaN(global_mouseEvent.KoefPixToMM))
@@ -27,6 +32,9 @@ function HitInBezier4(context, px, py, x0, y0, x1, y1, x2, y2, x3, y3)
     tx=x3-x0;
     ty=y3-y0;
 
+    if (Math.abs(tx*tx+ty*ty) < double_eps)
+        return HitInLine(context, px, py, x0, y0, x1, y1, x2, y2, x3 + hit_error_eps, y3 + hit_error_eps);
+
     d=1.5/Math.sqrt(tx*tx+ty*ty);
     if(global_mouseEvent !== null && typeof global_mouseEvent === "object" && typeof global_mouseEvent.KoefPixToMM === "number" && !isNaN(global_mouseEvent.KoefPixToMM))
         d *= global_mouseEvent.KoefPixToMM;
@@ -49,6 +57,9 @@ function HitInBezier3(context, px, py, x0, y0, x1, y1, x2, y2)
     var tx, ty, dx, dy, d;
     tx=x2-x0;
     ty=y2-y0;
+
+    if (Math.abs(tx*tx+ty*ty) < double_eps)
+        return HitInLine(context, px, py, x0, y0, x1, y1, x2 + hit_error_eps, y2 + hit_error_eps);
 
     d=1.5/Math.sqrt(tx*tx+ty*ty);
     if(global_mouseEvent !== null && typeof global_mouseEvent === "object" && typeof global_mouseEvent.KoefPixToMM === "number" && !isNaN(global_mouseEvent.KoefPixToMM))
