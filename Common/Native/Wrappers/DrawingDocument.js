1546462410b1a83776b70c971ff38d13b1b2ca6b
Oleg.Korshul <Oleg.Korshul@OnlyOffice.com>
2014-03-26 07:42:23 +0000

    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@55139 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/Native/Wrappers/DrawingDocument.js
+++ Common/Native/Wrappers/DrawingDocument.js
@@ -605,8 +605,13 @@ CDrawingDocument.prototype =
             }
 
             // теперь проверить трек таблиц
+            /*
             var ret = this.Native["checkMouseDown_Drawing"](pos.X, pos.Y, pos.Page);
             if (ret === true)
+                return;
+            */
+            var is_drawing = this.checkMouseDown_Drawing(pos);
+            if (is_drawing === true)
                 return;
 
             this.Native["DD_NeedScrollToTargetFlag"](true);
@@ -641,7 +646,12 @@ CDrawingDocument.prototype =
         // восстанавливаем фокус
         this.m_bIsMouseLock = false;
 
+        /*
         var is_drawing = this.Native["checkMouseUp_Drawing"](pos.X, pos.Y, pos.Page);
+        if (is_drawing === true)
+            return;
+        */
+        var is_drawing = this.checkMouseUp_Drawing(pos);
         if (is_drawing === true)
             return;
 
@@ -679,7 +689,13 @@ CDrawingDocument.prototype =
             this.SetCursorType("default");
 
         this.StartUpdateOverlay();
+
+        /*
         var is_drawing = this.Native["checkMouseMove_Drawing"](pos.X, pos.Y, pos.Page);
+        if (is_drawing === true)
+            return;
+        */
+        var is_drawing = this.checkMouseMove_Drawing(pos);
         if (is_drawing === true)
             return;
 
@@ -749,6 +765,260 @@ CDrawingDocument.prototype =
     },
     DrawGuiCanvasTextProps : function()
     {
+    },
+
+    // drawings mouse events
+    checkMouseDown_Drawing : function(pos)
+    {
+        /*
+        var _ret = this.TableOutlineDr.checkMouseDown(pos, oWordControl);
+        if (_ret === true)
+        {
+            oWordControl.m_oLogicDocument.Selection_Remove();
+            this.TableOutlineDr.bIsTracked = true;
+            this.LockCursorType("move");
+
+            this.TableOutlineDr.TableOutline.Table.Select_All();
+            this.TableOutlineDr.TableOutline.Table.Document_SetThisElementCurrent(true);
+
+            if (-1 == oWordControl.m_oTimerScrollSelect)
+            {
+                oWordControl.m_oTimerScrollSelect = setInterval(oWordControl.SelectWheel, 20);
+            }
+            oWordControl.EndUpdateOverlay();
+            return true;
+        }
+
+        if (this.FrameRect.IsActive)
+        {
+            var eps = 10 * g_dKoef_pix_to_mm * 100 / oWordControl.m_nZoomValue;
+            var _check = this.checkCursorOnTrackRect(pos.X, pos.Y, eps, this.FrameRect.Rect);
+
+            if (-1 != _check)
+            {
+                this.FrameRect.IsTracked = true;
+                this.FrameRect.Track.X = pos.X;
+                this.FrameRect.Track.Y = pos.Y;
+                this.FrameRect.Track.Type = _check;
+
+                switch (_check)
+                {
+                    case 0:
+                    {
+                        this.LockCursorType("nw-resize");
+                        break;
+                    }
+                    case 1:
+                    {
+                        this.LockCursorType("n-resize");
+                        break;
+                    }
+                    case 2:
+                    {
+                        this.LockCursorType("ne-resize");
+                        break;
+                    }
+                    case 3:
+                    {
+                        this.LockCursorType("e-resize");
+                        break;
+                    }
+                    case 4:
+                    {
+                        this.LockCursorType("se-resize");
+                        break;
+                    }
+                    case 5:
+                    {
+                        this.LockCursorType("s-resize");
+                        break;
+                    }
+                    case 6:
+                    {
+                        this.LockCursorType("sw-resize");
+                        break;
+                    }
+                    case 7:
+                    {
+                        this.LockCursorType("w-resize");
+                        break;
+                    }
+                    default:
+                    {
+                        this.LockCursorType("move");
+                        break;
+                    }
+                }
+
+                if (-1 == oWordControl.m_oTimerScrollSelect)
+                {
+                    oWordControl.m_oTimerScrollSelect = setInterval(oWordControl.SelectWheel, 20);
+                }
+                oWordControl.EndUpdateOverlay();
+                return true;
+            }
+        }
+        */
+
+        return false;
+    },
+
+    checkMouseMove_Drawing : function(pos)
+    {
+        /*
+        var oWordControl = this.m_oWordControl;
+        if (this.TableOutlineDr.bIsTracked)
+        {
+            this.TableOutlineDr.checkMouseMove(global_mouseEvent.X, global_mouseEvent.Y, oWordControl);
+            oWordControl.ShowOverlay();
+            oWordControl.OnUpdateOverlay();
+            oWordControl.EndUpdateOverlay();
+            return true;
+        }
+        */
+
+        if (this.InlineTextTrackEnabled)
+        {
+            this.InlineTextTrack        = this.LogicDocument.Get_NearestPos(pos.Page, pos.X, pos.Y);
+            this.InlineTextTrackPage    = pos.Page;
+
+            this.OnUpdateOverlay();
+            this.EndUpdateOverlay();
+            return true;
+        }
+
+        /*
+        if (this.FrameRect.IsActive)
+        {
+            if (!this.FrameRect.IsTracked && this.FrameRect.PageIndex == pos.Page)
+            {
+                var eps = 10 * g_dKoef_pix_to_mm * 100 / oWordControl.m_nZoomValue;
+                var _check = this.checkCursorOnTrackRect(pos.X, pos.Y, eps, this.FrameRect.Rect);
+
+                if (_check != -1)
+                {
+                    switch (_check)
+                    {
+                        case 0:
+                        {
+                            this.SetCursorType("nw-resize");
+                            break;
+                        }
+                        case 1:
+                        {
+                            this.SetCursorType("n-resize");
+                            break;
+                        }
+                        case 2:
+                        {
+                            this.SetCursorType("ne-resize");
+                            break;
+                        }
+                        case 3:
+                        {
+                            this.SetCursorType("e-resize");
+                            break;
+                        }
+                        case 4:
+                        {
+                            this.SetCursorType("se-resize");
+                            break;
+                        }
+                        case 5:
+                        {
+                            this.SetCursorType("s-resize");
+                            break;
+                        }
+                        case 6:
+                        {
+                            this.SetCursorType("sw-resize");
+                            break;
+                        }
+                        case 7:
+                        {
+                            this.SetCursorType("w-resize");
+                            break;
+                        }
+                        default:
+                        {
+                            this.SetCursorType("move");
+                            break;
+                        }
+                    }
+                    // оверлей не нужно перерисовывать
+                    oWordControl.EndUpdateOverlay();
+                    return true;
+                }
+            }
+            else
+            {
+                this.checkTrackRect(pos);
+
+                oWordControl.ShowOverlay();
+                oWordControl.OnUpdateOverlay();
+                oWordControl.EndUpdateOverlay();
+                return true;
+            }
+        }
+        */
+
+        return false;
+    },
+
+    checkMouseUp_Drawing : function(pos)
+    {
+        /*
+        var oWordControl = this.m_oWordControl;
+        if (this.TableOutlineDr.bIsTracked)
+        {
+            this.TableOutlineDr.checkMouseUp(global_mouseEvent.X, global_mouseEvent.Y, oWordControl);
+            oWordControl.m_oLogicDocument.Document_UpdateInterfaceState();
+            oWordControl.m_oLogicDocument.Document_UpdateRulersState();
+
+            if (-1 != oWordControl.m_oTimerScrollSelect)
+            {
+                clearInterval(oWordControl.m_oTimerScrollSelect);
+                oWordControl.m_oTimerScrollSelect = -1;
+            }
+            oWordControl.OnUpdateOverlay();
+
+            oWordControl.EndUpdateOverlay();
+            return true;
+        }
+        */
+
+        if (this.InlineTextTrackEnabled)
+        {
+            this.InlineTextTrack        = this.LogicDocument.Get_NearestPos(pos.Page, pos.X, pos.Y);
+            this.InlineTextTrackPage    = pos.Page;
+            this.EndTrackText();
+
+            this.OnUpdateOverlay();
+            this.EndUpdateOverlay();
+            return true;
+        }
+
+        /*
+        if (this.FrameRect.IsActive && this.FrameRect.IsTracked)
+        {
+            this.FrameRect.IsTracked = false;
+
+            this.checkTrackRect(pos);
+            var _track = this.FrameRect.Track;
+            this.FrameRect.Frame.Change_Frame(_track.L, _track.T, _track.R - _track.L, _track.B - _track.T, _track.PageIndex);
+
+            if (-1 != oWordControl.m_oTimerScrollSelect)
+            {
+                clearInterval(oWordControl.m_oTimerScrollSelect);
+                oWordControl.m_oTimerScrollSelect = -1;
+            }
+            oWordControl.OnUpdateOverlay();
+
+            oWordControl.EndUpdateOverlay();
+            return true;
+        }
+        */
+        return false;
     }
 };
 
