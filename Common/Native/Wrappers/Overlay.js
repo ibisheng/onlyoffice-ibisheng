aaf7e64d7a7481d6bffe3800331e681754e29c1c
Oleg.Korshul <Oleg.Korshul@OnlyOffice.com>
2014-04-24 06:38:53 +0000

    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@55667 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/Native/Wrappers/Overlay.js
+++ Common/Native/Wrappers/Overlay.js
@@ -113,5 +113,336 @@ CAutoshapeTrack.prototype =
     DrawPresentationComment : function(type, x, y, w, h)
     {
         this.Native["PD_DrawPresentationComment"](type, x, y, w, h);
+    },
+    
+    // repeat drawing interface
+    put_GlobalAlpha : function(enable, alpha)
+    {
+        this.Native["PD_put_GlobalAlpha"](enable, alpha);
+    },
+    Start_GlobalAlpha : function()
+    {
+        // nothing
+    },
+    End_GlobalAlpha : function()
+    {
+        this.Native["PD_End_GlobalAlpha"]();
+    },
+    // pen methods
+    p_color : function(r,g,b,a)
+    {
+        this.Native["PD_p_color"](r,g,b,a);
+    },
+    p_width : function(w)
+    {
+        this.Native["PD_p_width"](w);
+    },
+    // brush methods
+    b_color1 : function(r,g,b,a)
+    {
+        this.Native["PD_b_color1"](r,g,b,a);
+    },
+    b_color2 : function(r,g,b,a)
+    {
+        this.Native["PD_b_color2"](r,g,b,a);
+    },
+
+    transform : function(sx,shy,shx,sy,tx,ty)
+    {
+        this.Native["PD_transform"](sx,shy,shx,sy,tx,ty);
+    },
+    // path commands
+    _s : function()
+    {
+        this.Native["PD_PathStart"]();
+    },
+    _e : function()
+    {
+        this.Native["PD_PathEnd"]();
+    },
+    _z : function()
+    {
+        this.Native["PD_PathClose"]();
+    },
+    _m : function(x,y)
+    {
+        this.Native["PD_PathMoveTo"](x,y);
+    },
+    _l : function(x,y)
+    {
+        this.Native["PD_PathLineTo"](x,y);
+    },
+    _c : function(x1,y1,x2,y2,x3,y3)
+    {
+        this.Native["PD_PathCurveTo"](x1,y1,x2,y2,x3,y3);
+    },
+    _c2 : function(x1,y1,x2,y2)
+    {
+        this.Native["PD_PathCurveTo2"](x1,y1,x2,y2);
+    },
+    ds : function()
+    {
+        this.Native["PD_Stroke"]();
+    },
+    df : function()
+    {
+        this.Native["PD_Fill"]();
+    },
+
+    // canvas state
+    save : function()
+    {
+        this.Native["PD_Save"]();
+    },
+    restore : function()
+    {
+        this.Native["PD_Restore"]();
+    },
+    clip : function()
+    {
+        this.Native["PD_clip"]();
+    },
+
+    reset : function()
+    {
+        this.Native["PD_reset"]();
+    },
+
+    transform3 : function(m, isNeedInvert)
+    {
+        this.Native["PD_transform3"](m.sx,m.shy,m.shx,m.sy,m.tx,m.ty,isNeedInvert);
+    },
+
+    FreeFont : function()
+    {
+    },
+
+    // images
+    drawImage : function(img,x,y,w,h,alpha,srcRect)
+    {
+        if (!srcRect)
+            return this.Native["PD_drawImage"](img,x,y,w,h,alpha);
+
+        return this.Native["PD_drawImage"](img,x,y,w,h,alpha,srcRect.l,srcRect.t,srcRect.r,srcRect.b);
+    },
+
+    // text
+    GetFont : function()
+    {
+    },
+    font : function(font_id,font_size)
+    {
+    },
+    SetFont : function(font)
+    {
+    },
+
+    SetTextPr : function(textPr, theme)
+    {
+    },
+    GetTextPr : function()
+    {
+    },
+
+    SetFontSlot : function(slot, fontSizeKoef)
+    {
+    },
+
+    FillText : function(x,y,text)
+    {
+    },
+    t : function(text,x,y)
+    {
+    },
+    FillText2 : function(x,y,text,cropX,cropW)
+    {
+    },
+    t2 : function(text,x,y,cropX,cropW)
+    {
+    },
+    FillTextCode : function(x,y,lUnicode)
+    {
+    },
+    tg : function(text,x,y)
+    {
+    },
+    charspace : function(space)
+    {
+    },
+
+    SetIntegerGrid : function(param)
+    {
+        //this.Native["PD_SetIntegerGrid"](param);
+    },
+    GetIntegerGrid : function()
+    {
+        //return this.m_bIntegerGrid;
+        return false;
+    },
+
+    DrawHeaderEdit : function(yPos, lock_type)
+    {
+        this.Native["PD_DrawHeaderEdit"](yPos, lock_type);
+    },
+
+    DrawFooterEdit : function(yPos, lock_type)
+    {
+        this.Native["PD_DrawFooterEdit"](yPos, lock_type);
+    },
+
+    DrawLockParagraph : function(lock_type, x, y1, y2)
+    {
+        this.Native["PD_DrawLockParagraph"](lock_type, x, y1, y2);
+    },
+
+    DrawLockObjectRect : function(lock_type, x, y, w, h)
+    {
+        this.Native["PD_DrawLockObjectRect"](lock_type, x, y, w, h);
+    },
+
+    DrawEmptyTableLine : function(x1,y1,x2,y2)
+    {
+        this.Native["PD_DrawEmptyTableLine"](x1,y1,x2,y2);
+    },
+
+    DrawSpellingLine : function(y0, x0, x1, w)
+    {
+        this.Native["PD_DrawSpellingLine"](y0, x0, x1, w);
+    },
+
+    // smart methods for horizontal / vertical lines
+    drawHorLine : function(align, y, x, r, penW)
+    {
+        this.Native["PD_drawHorLine"](align, y, x, r, penW);
+    },
+    drawHorLine2 : function(align, y, x, r, penW)
+    {
+        this.Native["PD_drawHorLine2"](align, y, x, r, penW);
+    },
+    drawVerLine : function(align, x, y, b, penW)
+    {
+        this.Native["PD_drawVerLine"](align, x, y, b, penW);
+    },
+
+    // мега крутые функции для таблиц
+    drawHorLineExt : function(align, y, x, r, penW, leftMW, rightMW)
+    {
+        this.Native["PD_drawHorLineExt"](align, y, x, r, penW, leftMW, rightMW);
+    },
+
+    rect : function(x,y,w,h)
+    {
+        this.Native["PD_rect"](x,y,w,h);
+    },
+
+    TableRect : function(x,y,w,h)
+    {
+        this.Native["PD_TableRect"](x,y,w,h);
+    },
+
+    // функции клиппирования
+    AddClipRect : function(x, y, w, h)
+    {
+        this.Native["PD_AddClipRect"](x,y,w,h);
+    },
+    RemoveClipRect : function()
+    {
+        // nothing
+    },
+
+    SetClip : function(r)
+    {
+        this.Native["PD_SetClip"](r.x, r.y, r.w, r.h);
+    },
+
+    RemoveClip : function()
+    {
+        this.Native["PD_RemoveClip"]();
+    },
+
+    drawCollaborativeChanges : function(x, y, w, h)
+    {
+        this.Native["PD_drawCollaborativeChanges"](x, y, w, h);
+    },
+
+    drawSearchResult : function(x, y, w, h)
+    {
+        this.Native["PD_drawSearchResult"](x, y, w, h);
+    },
+
+    SavePen : function()
+    {
+        this.Native["PD_SavePen"]();
+    },
+    RestorePen : function()
+    {
+        this.Native["PD_RestorePen"]();
+    },
+
+    SaveBrush : function()
+    {
+        this.Native["PD_SaveBrush"]();
+    },
+    RestoreBrush : function()
+    {
+        this.Native["PD_RestoreBrush"]();
+    },
+
+    SavePenBrush : function()
+    {
+        this.Native["PD_SavePenBrush"]();
+    },
+    RestorePenBrush : function()
+    {
+        this.Native["PD_RestorePenBrush"]();
+    },
+
+    SaveGrState : function()
+    {
+        this.Native["PD_SaveGrState"]();
+    },
+    RestoreGrState : function()
+    {
+        this.Native["PD_RestoreGrState"]();
+    },
+
+    StartClipPath : function()
+    {
+        this.Native["PD_StartClipPath"]();
+    },
+
+    EndClipPath : function()
+    {
+        this.Native["PD_EndClipPath"]();
+    },
+
+    StartCheckTableDraw : function()
+    {
+        return this.Native["PD_StartCheckTableDraw"]();
+    },
+
+    EndCheckTableDraw : function(bIsRestore)
+    {
+        return this.Native["PD_EndCheckTableDraw"](bIsRestore);
+    },
+
+    SetTextClipRect : function(_l, _t, _r, _b)
+    {
+        return this.Native["PD_SetTextClipRect"](_l, _t, _r, _b);
+    },
+
+    AddSmartRect : function(x, y, w, h, pen_w)
+    {
+        return this.Native["PD_AddSmartRect"](x, y, w, h, pen_w);
+    },
+
+    Drawing_StartCheckBounds : function(x, y, w, h)
+    {
+
+    },
+
+    Drawing_EndCheckBounds : function()
+    {
+
     }
 };
