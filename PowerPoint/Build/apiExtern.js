b04f1063e696522e9dc0083836ff10def1087c6a
Alexey.Golubev <Alexey.Golubev@OnlyOffice.com>
2014-04-25 12:25:43 +0000

    Удалены не используемые ресурсы.
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@55712 954022d7-b5bf-4e40-9824-e11837661b57

--- PowerPoint/Build/apiExtern.js
+++
@@ -1,8 +0,0 @@
-//Tihs file containes definition of object which used in api.js
-//It need to prevent minimize the name of object's method.
-
-//WaitDownload(){};
-//function Search(text){};
-//function startSearch(){};
-//function stopSearch(){};
-function $(){};
