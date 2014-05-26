34ab768508a60d4bad0da7b9a4f159d1e73873e2
Sergey.Konovalov <Sergey.Konovalov@OnlyOffice.com>
2014-05-26 06:24:53 +0000

    ошибки при автозаполении, если последние столбцы были полностью пустыми.
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@56283 954022d7-b5bf-4e40-9824-e11837661b57

--- Excel/model/Workbook.js
+++ Excel/model/Workbook.js
@@ -7627,15 +7627,21 @@ function PromoteHelper(bVerical, bReverse, bbox){
 	this.bReverse = bReverse;
 	this.bbox = bbox;
 	this.oDataRow = {};
-	this.nDataRowMaxIndex = -1;
 	//для get
 	this.oCurRow = null;
 	this.nCurColIndex = null;
+	this.nRowLength = 0;
 	this.nColLength = 0;
 	if(this.bVerical)
+	{
+		this.nRowLength = this.bbox.c2 - this.bbox.c1 + 1;
 		this.nColLength = this.bbox.r2 - this.bbox.r1 + 1;
+	}
 	else
+	{
+		this.nRowLength = this.bbox.r2 - this.bbox.r1 + 1;
 		this.nColLength = this.bbox.c2 - this.bbox.c1 + 1;
+	}
 }
 PromoteHelper.prototype = {
 	add: function(nRow, nCol, nVal, bDelimiter, sPrefix, bDate, oAdditional){
@@ -7653,8 +7659,6 @@ PromoteHelper.prototype = {
 		{
 			row = {};
 			this.oDataRow[nRow] = row;
-			if(this.nDataRowMaxIndex < nRow)
-				this.nDataRowMaxIndex = nRow;
 		}
 		row[nCol] = {nCol: nCol, nVal: nVal, bDelimiter: bDelimiter, sPrefix: sPrefix, bDate: bDate, oAdditional: oAdditional, oSequence: null, nCurValue: null};
 	},
@@ -7948,8 +7952,8 @@ PromoteHelper.prototype = {
 		}
 	},
 	setIndex: function(index){
-		if(-1 != this.nDataRowMaxIndex && this.nDataRowMaxIndex < index)
-			index = index % (this.nDataRowMaxIndex + 1);
+		if(0 != this.nRowLength && index >= this.nRowLength)
+			index = index % (this.nRowLength);
 		this.oCurRow = this.oDataRow[index];
 		this.nCurColIndex = 0;
 	},
