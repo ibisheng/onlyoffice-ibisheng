e1dda2cb57680c9d921f551590d7b65637281937
Alexander.Trofimov <Alexander.Trofimov@OnlyOffice.com>
2014-06-25 08:23:37 +0000

    Правка бага http://bugzserver/show_bug.cgi?id=24431 Задание по умолчанию freeze значений.
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@56951 954022d7-b5bf-4e40-9824-e11837661b57

--- Excel/view/WorksheetView.js
+++ Excel/view/WorksheetView.js
@@ -3251,7 +3251,10 @@
 				var col, row;
 				if (null !== t.topLeftFrozenCell)
 					col = row = 0;
-				else {
+				else if (0 === ar.startCol && 0 === ar.startRow) {
+					col = (t.visibleRange.c2 - t.visibleRange.c1) / 2;
+					row = (t.visibleRange.r2 - t.visibleRange.r1) / 2;
+				} else {
 					col = ar.startCol;
 					row = ar.startRow;
 				}
