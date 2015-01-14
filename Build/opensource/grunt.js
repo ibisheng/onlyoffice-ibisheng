3e7f137ed45b6f2c7b909000887013239fba6605
Alexey.Golubev <Alexey.Golubev@OnlyOffice.com>
2015-01-14 14:18:19 +0000

    Смержены изменения с бранча.
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@60494 954022d7-b5bf-4e40-9824-e11837661b57

--- Build/opensource/grunt.js
+++ Build/opensource/grunt.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt) {
     var defaultConfig, packageFile;
 
-    grunt.loadNpmTasks('grunt-contrib');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-compress');
 
     grunt.registerTask('build_webword_init', 'Initialize build WebWord SDK.', function(){
         defaultConfig = './webword.json';
@@ -54,9 +56,34 @@ module.exports = function(grunt) {
             pkg: '<json:' + defaultConfig + '>',
             meta: {
                 banner: '/*\n' +
-                    ' * Copyright (c) Ascensio System SIA <%= grunt.template.today("yyyy") %>. All rights reserved\n' +
+                    ' * (c) Copyright Ascensio System SIA 2010-<%= grunt.template.today("yyyy") %>\n' +
                     ' *\n' +
-                    ' * <%= pkg.info.homepage %> \n' +
+                    ' * This program is a free software product. You can redistribute it and/or \n' +
+                    ' * modify it under the terms of the GNU Affero General Public License (AGPL) \n' +
+                    ' * version 3 as published by the Free Software Foundation. In accordance with \n' +
+                    ' * Section 7(a) of the GNU AGPL its Section 15 shall be amended to the effect \n' +
+                    ' * that Ascensio System SIA expressly excludes the warranty of non-infringement\n' +
+                    ' * of any third-party rights.\n' +
+                    ' *\n' +
+                    ' * This program is distributed WITHOUT ANY WARRANTY; without even the implied \n' +
+                    ' * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For \n' +
+                    ' * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html\n' +
+                    ' *\n' +
+                    ' * You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia,\n' +
+                    ' * EU, LV-1021.\n' +
+                    ' *\n' +
+                    ' * The  interactive user interfaces in modified source and object code versions\n' +
+                    ' * of the Program must display Appropriate Legal Notices, as required under \n' +
+                    ' * Section 5 of the GNU AGPL version 3.\n' +
+                    ' *\n' +
+                    ' * Pursuant to Section 7(b) of the License you must retain the original Product\n' +
+                    ' * logo when distributing the program. Pursuant to Section 7(e) we decline to\n' +
+                    ' * grant you any rights under trademark law for use of our trademarks.\n' +
+                    ' *\n' +
+                    ' * All the Product\'s GUI elements, including illustrations and icon sets, as\n' +
+                    ' * well as technical writing content are licensed under the terms of the\n' +
+                    ' * Creative Commons Attribution-ShareAlike 4.0 International. See the License\n' +
+                    ' * terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode\n' +
                     ' *\n' +
                     ' * Version: <%= pkg.info.version %> (build:<%= pkg.info.build %>)\n' +
                     ' */'
