0ae7233d266dbf4acc889a8ac8564ad7540230f1
Oleg.Korshul <Oleg.Korshul@OnlyOffice.com>
2015-03-18 12:14:41 +0000

    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@61481 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/Native/Wrappers/DrawingDocument.js
+++ Common/Native/Wrappers/DrawingDocument.js
@@ -571,7 +571,7 @@ function CDrawingDocument()
     this.TargetShowNeedFlag     = false;
 
     this.m_bIsSelection         = false;
-    this.m_bIsMouseLock         = false;
+    this.m_bIsMouseLockDocument = false;
 
     this.IsKeyDownButNoPress    = false;
     this.bIsUseKeyPress         = false;
@@ -1409,23 +1409,23 @@ CDrawingDocument.prototype =
 
     LogicDocumentOnMouseDown : function(e, x, y, page)
     {
-        if (this.m_bIsMouseLock)
+        if (this.m_bIsMouseLockDocument)
         {
             this.LogicDocument.OnMouseUp(e, x, y, page);
-            this.m_bIsMouseLock = false;
+            this.m_bIsMouseLockDocument = false;
         }
         this.LogicDocument.OnMouseDown(e, x, y, page);
-        this.m_bIsMouseLock = true;
+        this.m_bIsMouseLockDocument = true;
     },
     LogicDocumentOnMouseUp : function(e, x, y, page)
     {
-        if (!this.m_bIsMouseLock)
+        if (!this.m_bIsMouseLockDocument)
         {
             this.LogicDocument.OnMouseDown(e, x, y, page);
-            this.m_bIsMouseLock = true;
+            this.m_bIsMouseLockDocument = true;
         }
         this.LogicDocument.OnMouseUp(e, x, y, page);
-        this.m_bIsMouseLock = false;
+        this.m_bIsMouseLockDocument = false;
     },
 
     OnCheckMouseDown : function(e)
@@ -1633,7 +1633,7 @@ CDrawingDocument.prototype =
                 return;
 
             this.Native["DD_NeedScrollToTargetFlag"](true);
-            this.LogicDocumentOnMouseDown.OnMouseDown(global_mouseEvent, pos.X, pos.Y, pos.Page);
+            this.LogicDocumentOnMouseDown(global_mouseEvent, pos.X, pos.Y, pos.Page);
             this.Native["DD_NeedScrollToTargetFlag"](false);
         }
 
