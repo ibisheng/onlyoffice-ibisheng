1ed2eab4076ecbf7e577c8175ac8703c5477749f
Oleg.Korshul <Oleg.Korshul@OnlyOffice.com>
2015-06-15 14:36:02 +0000

    правки для подбора шрифтов (десктопный редактор)
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@63036 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/FontsFreeType/font_map.js
+++ Common/FontsFreeType/font_map.js
@@ -819,13 +819,21 @@ CFontSelect.prototype =
             _len = fs.GetLong();
             this.m_wsFontPath = fs.GetString(_len >> 1);
 
-            // удаляем все, кроме имени файла
-            var _found1 = this.m_wsFontPath.lastIndexOf("/");
-            var _found2 = this.m_wsFontPath.lastIndexOf("\\");
-            var _found = Math.max(_found1, _found2);
+            if (undefined === window["AscDesktopEditor"])
+            {
+                // удаляем все, кроме имени файла
+                var _found1 = this.m_wsFontPath.lastIndexOf("/");
+                var _found2 = this.m_wsFontPath.lastIndexOf("\\");
+                var _found = Math.max(_found1, _found2);
 
-            if (0 <= _found)
-                this.m_wsFontPath = this.m_wsFontPath.substring(_found + 1);
+                if (0 <= _found)
+                    this.m_wsFontPath = this.m_wsFontPath.substring(_found + 1);
+            }
+            else
+            {
+                this.m_wsFontPath = this.m_wsFontPath.replace(/\\\\/g, "\\");
+                this.m_wsFontPath = this.m_wsFontPath.replace(/\\/g, "/");
+            }
         }
 
         this.m_lIndex = fs.GetLong();
