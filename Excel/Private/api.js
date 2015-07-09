9376afd10669df7c503335ffb464e2c3473f153f
Alexander.Trofimov <Alexander.Trofimov@OnlyOffice.com>
2015-07-09 14:37:04 +0000

    Merged revision(s) r63026 from AVS/Sources/TeamlabOffice/branches/TeamlabOffice_v3.0_Postgresql/OfficeWeb* Добавил недостающие файлы
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@63432 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ Excel/Private/api.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+/* api.js
+ *
+ * Author: Alexander.Trofimov@avsmedia.net
+ * Date:   Apr 23, 2015
+ */
+(
+	/**
+	 * @param {jQuery} $
+	 * @param {Window} window
+	 * @param {undefined} undefined
+	 */
+		function ($, window, undefined) {
+
+		var asc = window["Asc"];
+
+		asc['spreadsheet_api'].prototype.asc_addComment = function (oComment) {
+			if (oComment.bDocument)
+				this.wb.cellCommentator.asc_addComment(oComment);
+			else {
+				var ws = this.wb.getWorksheet();
+				ws.cellCommentator.asc_addComment(oComment);
+			}
+		};
+	}
+	)(jQuery, window);
