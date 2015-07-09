74cf207c8b7d05fa81675911863aad8792d3123c
Alexander.Trofimov <Alexander.Trofimov@OnlyOffice.com>
2015-07-09 14:30:14 +0000

    Merged revision(s) r62980 from AVS/Sources/TeamlabOffice/branches/TeamlabOffice_v3.0_Postgresql/OfficeWeb* Добавил Private/api.js Перенес туда Word asc_addComment
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@63429 954022d7-b5bf-4e40-9824-e11837661b57

--- Word/api.js
+++ Word/api.js
@@ -5581,25 +5581,6 @@ asc_docs_api.prototype.asc_hideComments = function()
 
 asc_docs_api.prototype.asc_addComment = function(AscCommentData)
 {
-    if (true === CollaborativeEditing.Get_GlobalLock())
-        return;
-
-    if (null == this.WordControl.m_oLogicDocument)
-        return;
-
-    // Комментарий без цитаты позволяем добавить всегда
-    if (true !== this.can_AddQuotedComment() || false === this.WordControl.m_oLogicDocument.Document_Is_SelectionLocked(changestype_Paragraph_Content))
-    {
-        var CommentData = new CCommentData();
-        CommentData.Read_FromAscCommentData(AscCommentData);
-
-        this.WordControl.m_oLogicDocument.Create_NewHistoryPoint(historydescription_Document_AddComment);
-        var Comment = this.WordControl.m_oLogicDocument.Add_Comment(CommentData);
-        if (null != Comment)
-            this.sync_AddComment(Comment.Get_Id(), CommentData);
-
-        return Comment.Get_Id();
-    }
 };
 
 asc_docs_api.prototype.asc_removeComment = function(Id)
