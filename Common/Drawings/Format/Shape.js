0e13d1494027c08f8d1fcb0e62cc9e9a069982e8
Sergey.Luzyanin <Sergey.Luzyanin@OnlyOffice.com>
2015-08-10 15:28:48 +0000

    баг с клиппированием
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@63996 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/Drawings/Format/Shape.js
+++ Common/Drawings/Format/Shape.js
@@ -1941,9 +1941,9 @@ CShape.prototype =
 
             oClipRect = {
                 x: -1,
-                y: -_vertical_shift - t_ins,
+                y: - 1,
                 w: Math.max(this.extX, this.extY) + 2,
-                h: this.contentHeight + (b_ins + t_ins)
+                h: this.contentHeight + (b_ins + t_ins) + 2
             };
         }
         return oClipRect;
