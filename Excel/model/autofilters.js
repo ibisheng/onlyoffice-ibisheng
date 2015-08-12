15d01c82947f3165c47a2f5544bfeddaec10e52d
Igor.Zotov <Igor.Zotov@OnlyOffice.com>
2015-08-12 14:34:18 +0000

    http://bugzserver/show_bug.cgi?id=27254 - Раскрытие скрытой строки фильтрованного диапазона после вставки форматированной таблицы и снятия с нее автофильтра
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@64038 954022d7-b5bf-4e40-9824-e11837661b57

--- Excel/model/autofilters.js
+++ Excel/model/autofilters.js
@@ -3702,7 +3702,10 @@ var maxIndividualValues = 10000;
 			_openHiddenRows: function(filter)
 			{
 				var aWs = this._getCurrentWS();
-				if(filter && filter.Ref)
+				var autoFilter = filter.AutoFilter !== undefined ? filter.AutoFilter : filter;
+				var isApplyFilter = autoFilter.FilterColumns && autoFilter.FilterColumns.length ? true : false;
+				
+				if(filter && filter.Ref && isApplyFilter)
 				{
 					aWs.setRowHidden(false, filter.Ref.r1, filter.Ref.r2);
 				}
