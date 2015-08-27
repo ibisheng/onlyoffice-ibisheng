f4b14268a7e16274bab84f300c8a37ce133ea501
Alexander.Trofimov <Alexander.Trofimov@OnlyOffice.com>
2015-08-27 07:26:59 +0000

    export
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@64347 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/editorscommon.js
+++ Common/editorscommon.js
@@ -4,11 +4,13 @@ if (typeof String.prototype.startsWith != 'function') {
 	String.prototype.startsWith = function (str){
 		return this.indexOf(str) === 0;
 	};
+	String.prototype['startsWith'] = String.prototype.startsWith;
 }
 if (typeof String.prototype.endsWith !== 'function') {
 	String.prototype.endsWith = function(suffix) {
 		return this.indexOf(suffix, this.length - suffix.length) !== -1;
 	};
+	String.prototype['endsWith'] = String.prototype.endsWith;
 }
 
 var g_sMainServiceLocalUrl = "/CanvasService.ashx";
