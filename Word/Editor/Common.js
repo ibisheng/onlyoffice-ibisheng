92ef2bafb2a29161e916b6467ea8f86a78cd364c
Alexander.Trofimov <Alexander.Trofimov@OnlyOffice.com>
2015-08-28 13:12:05 +0000

    delete Common_CopyObj2
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@64384 954022d7-b5bf-4e40-9824-e11837661b57

--- Word/Editor/Common.js
+++ Word/Editor/Common.js
@@ -27,38 +27,3 @@ function Common_CopyObj(Obj)
     }
     return c;
 };
-
-function Common_CopyObj2(Dst, Obj)
-{
-    if( !Obj || !('object' == typeof(Obj) || 'array' == typeof(Obj)) )
-    {
-        return;
-    }
-
-    if (Dst == null)
-        Dst = {};
-
-    var p, v;
-    for(p in Obj)
-    {
-        if(Obj.hasOwnProperty(p))
-        {
-            v = Obj[p];
-            if(v && 'object' === typeof v )
-            {
-                if ( "object" != typeof(Dst[p]) )
-                {
-                    if ( "undefined" != typeof(v.splice) )
-                        Dst[p] = [];
-                    else
-                        Dst[p] = {};
-                }
-                Common_CopyObj2(Dst[p], v);
-            }
-            else
-            {
-                Dst[p] = v;
-            }
-        }
-    }
-}
