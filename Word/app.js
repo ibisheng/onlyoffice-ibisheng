cfb61dd1dbedd1d733f19ab7d1bdf1cdfd33f8ae
Ilya.Kirillov <Ilya.Kirillov@OnlyOffice.com>
2015-09-09 10:05:08 +0000

    Пока рецензирование не добавлено в меню в основную ветку, убрал ссылки на нужный файлы.
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@64582 954022d7-b5bf-4e40-9824-e11837661b57

--- Word/app.js
+++ Word/app.js
@@ -130,7 +130,7 @@ require([
             ,'Common.Controllers.ExternalDiagramEditor'
             /** proprietary end **/
 			,'Common.Controllers.ExternalMergeEditor'
-            ,'Common.Controllers.ReviewChanges'
+            //,'Common.Controllers.ReviewChanges'
         ]
     });
 
@@ -163,7 +163,7 @@ require([
         ,'common/main/lib/controller/ExternalDiagramEditor'
         /** proprietary end **/
 		,'common/main/lib/controller/ExternalMergeEditor'
-        ,'common/main/lib/controller/ReviewChanges'
+        //,'common/main/lib/controller/ReviewChanges'
     ], function() {
         app.start();
     });
