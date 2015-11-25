422d85439213bc6fd18ebfbf25bbf3a6a9b94bee
Alexander.Trofimov <Alexander.Trofimov@OnlyOffice.com>
2015-11-25 08:49:17 +0000

    delete window.CColor
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@66102 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/apiCommon.js
+++ Common/apiCommon.js
@@ -137,8 +137,7 @@
 		 * Export
 		 * -----------------------------------------------------------------------------
 		 */
-		window["CColor"] = window.CColor = CColor;
-		window.CColor;
+		window["CColor"] = CColor;
 		
 		prot = CColor.prototype;
 		prot["getR"]	= prot.getR;
