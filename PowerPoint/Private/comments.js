7c15b58a2d80d4a2aedfb3758e4e83646ddb4ab9
Alexander.Trofimov <Alexander.Trofimov@OnlyOffice.com>
2015-11-25 15:12:33 +0000

    Вынес license и comments в private
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@66122 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ PowerPoint/Private/comments.js
@@ -0,0 +1,18 @@
+"use strict";
+
+asc_docs_api.prototype.asc_addComment = function(AscCommentData) {
+  //if ( true === CollaborativeEditing.Get_GlobalLock() )
+  //   return;
+
+  if (null == this.WordControl.m_oLogicDocument) {
+    return;
+  }
+
+  var CommentData = new CCommentData();
+  CommentData.Read_FromAscCommentData(AscCommentData);
+
+  var Comment = this.WordControl.m_oLogicDocument.Add_Comment(CommentData);
+  if (Comment) {
+    return Comment.Get_Id();
+  }
+};
---
+++ Word/Private/license.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+asc_docs_api.prototype.asc_getEditorPermissions = function(licenseUrl, customerId) {
+  var t = this;
+  if (this.DocInfo && this.DocInfo.get_Id()) {
+
+    var sUserFirstName = null, sUserLastName = null;
+    var oUserInfo = this.DocInfo.get_UserInfo();
+    if (oUserInfo) {
+      sUserFirstName = oUserInfo.get_FirstName();
+      sUserLastName = oUserInfo.get_LastName();
+    }
+    CheckLicense(licenseUrl, customerId, this.DocInfo.get_UserId(), sUserFirstName, sUserLastName, function(err, res) {
+      t._onCheckLicenseEnd(err, res);
+    });
+  } else {
+    // Фиктивный вызов
+    this._onCheckLicenseEnd(true, false);
+  }
+  this._coAuthoringInit();
+};
+asc_docs_api.prototype._onCheckLicenseEnd = function(err, res) {
+  this.licenseResult = {err: err, res: res};
+  this._onEndPermissions();
+};
+asc_docs_api.prototype._onEndPermissions = function() {
+  if (null !== this.licenseResult && this.isOnFirstConnectEnd) {
+    var oResult = new window['Asc'].asc_CAscEditorPermissions();
+    oResult.asc_setCanLicense(this.licenseResult.res);
+    this.asc_fireCallback('asc_onGetEditorPermissions', oResult);
+  }
+};
