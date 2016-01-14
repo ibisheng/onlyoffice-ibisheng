2b133b3beb46415010b4638106f2931967f8f5f6
Anna.Pavlova <Anna.Pavlova@OnlyOffice.com>
2016-01-14 13:02:31 +0000

    Поправила баг с Gaps у Delimiters, менялись Gaps в зависимотсти от высоты контента Delimiters, высота контента вычислялась после вычисления Gaps (Gaps в ф-ии PreRecalc, высота контента в ф-ии Recalculate_Range)
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@67463 954022d7-b5bf-4e40-9824-e11837661b57

--- Word/Math/mathContent.js
+++ Word/Math/mathContent.js
@@ -535,7 +535,7 @@ CMathGapsInfo.prototype =
     {
         var kind       = Comp.kind;
 
-        var bEmptyGaps = kind == MATH_MATRIX || (kind == MATH_DELIMITER && Comp.Is_EmptyGaps()),
+        var bEmptyGaps = kind == MATH_MATRIX /*|| (kind == MATH_DELIMITER && Comp.Is_EmptyGaps())*/,
             bChildGaps = kind == MATH_DEGREE || kind == MATH_DEGREESubSup || kind == MATH_ACCENT || kind == MATH_RADICAL || kind == MATH_LIMIT || kind == MATH_BORDER_BOX;
 
 
