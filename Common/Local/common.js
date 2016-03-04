f506cdc6289d347cad32f6456af38b54e64ffe32
Oleg.Korshul <Oleg.Korshul@OnlyOffice.com>
2016-03-04 14:46:09 +0000

    новая схема лицензирования
    git-svn-id: svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk/OfficeWeb@68708 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/Local/common.js
+++ Common/Local/common.js
@@ -5,6 +5,17 @@ function CheckLicense(licenseUrl, customerId, userId, userFirstName, userLastNam
     callback(true, g_oLicenseResult.Success);
 }
 
+baseEditorsApi.prototype._onEndPermissions = function() 
+{
+	if (this.isOnFirstConnectEnd && this.isOnLoadLicense) 
+	{
+		var oResult = new window['Asc'].asc_CAscEditorPermissions();
+		oResult.asc_setCanLicense(true);
+		oResult.asc_setCanBranding(true);
+ 		this.sendEvent('asc_onGetEditorPermissions', oResult);
+	}
+};
+
 /////////////////////////////////////////////////////////
 //////////////       FONTS       ////////////////////////
 /////////////////////////////////////////////////////////
