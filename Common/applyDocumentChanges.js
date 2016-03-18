1ea876bb1c5e115f80eb279b9d38f439826c10b6
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2016-03-18 13:13:30 +0300

    add applyDocumentChanges.js

---
+++ Common/applyDocumentChanges.js
@@ -0,0 +1,4 @@
+window['AscApplyChanges'] = true;	// Флаг для проверки
+window['AscChanges'] = [
+
+];			// Массив строк изменений - изменения всех пользователей [[changes_1],[changes_2],..,[changes_n]]
