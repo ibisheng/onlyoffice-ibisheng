d2d2f14ea41b501cdd6e04642454070e762861f3
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2016-04-22 15:03:20 +0300

    move sdk-all.js, native.js, jquery_native.js to folder for test mobile resources remove FontsFreeType from mobile sdk

--- build/build-mobile.bat
+++ build/build-mobile.bat
@@ -2,4 +2,7 @@ CD /D %~dp0
 call npm install -g grunt-cli
 call npm install
 call grunt --level=WHITESPACE_ONLY --nomap=true --mobile=true --formatting=PRETTY_PRINT
+move ..\slide\sdk-all.js ..\..\core\test_mobile\sdk-all.js
+copy ..\common\Native\native.js  ..\..\core\test_mobile\native.js
+copy ..\common\Native\jquery_native.js  ..\..\core\test_mobile\jquery_native.js
 pause
