c86a5ac996078af893e334db1f6d1c7d3e4887f9
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2016-04-26 15:14:54 +0300

    asc -> AscCommonExcel

--- cell/model/CollaborativeEditing.js
+++ cell/model/CollaborativeEditing.js
@@ -339,7 +339,7 @@
 		};
 
 		CCollaborativeEditing.prototype.getLockInfo = function (typeElem, subType, sheetId, info) {
-			var oLockInfo = new asc.asc_CLockInfo();
+			var oLockInfo = new AscCommonExcel.asc_CLockInfo();
 			oLockInfo["sheetId"] = sheetId;
 			oLockInfo["type"] = typeElem;
 			oLockInfo["subType"] = subType;
