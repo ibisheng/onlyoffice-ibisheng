fef2026636ad4c939e7f1b054c87938551125ce1
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2016-04-28 14:44:46 +0300

    некрасивый коммит, но мне так удобнее

--- build/build-desktop.bat
+++ build/build-desktop.bat
@@ -1,10 +1,21 @@
 CD /D %~dp0
 call npm install -g grunt-cli
 call npm install
-call grunt --level=WHITESPACE_ONLY --desktop=true --formatting=PRETTY_PRINT
-rem call grunt --level=ADVANCED --desktop=true
+rem call grunt --level=WHITESPACE_ONLY --desktop=true --formatting=PRETTY_PRINT
+call grunt --level=ADVANCED --desktop=true
 copy ..\word\sdk-all.js ..\..\core\build\jsdesktop\word\sdk-all.js
 copy ..\slide\sdk-all.js ..\..\core\build\jsdesktop\slide\sdk-all.js
 copy ..\cell\sdk-all.js ..\..\core\build\jsdesktop\cell\sdk-all.js
 copy ..\common\Native\native.js ..\..\core\build\jsdesktop\common\Native\native.js
+
+copy ..\word\sdk-all.js ..\..\core\DesktopEditor\ChromiumBasedEditors2\app\test\src\build\win_64\Debug\Local\editors\sdkjs\word\sdk-all.js
+copy ..\slide\sdk-all.js ..\..\core\DesktopEditor\ChromiumBasedEditors2\app\test\src\build\win_64\Debug\Local\editors\sdkjs\slide\sdk-all.js
+copy ..\cell\sdk-all.js ..\..\core\DesktopEditor\ChromiumBasedEditors2\app\test\src\build\win_64\Debug\Local\editors\sdkjs\cell\sdk-all.js
+copy ..\common\Native\native.js ..\..\core\DesktopEditor\ChromiumBasedEditors2\app\test\src\build\win_64\Debug\Local\editors\sdkjs\common\Native\native.js
+
+copy ..\word\sdk-all.js ..\..\core\DesktopEditor\ChromiumBasedEditors2\app\test\src\build\win_64\Release\Local\editors\sdkjs\word\sdk-all.js
+copy ..\slide\sdk-all.js ..\..\core\DesktopEditor\ChromiumBasedEditors2\app\test\src\build\win_64\Release\Local\editors\sdkjs\slide\sdk-all.js
+copy ..\cell\sdk-all.js ..\..\core\DesktopEditor\ChromiumBasedEditors2\app\test\src\build\win_64\Release\Local\editors\sdkjs\cell\sdk-all.js
+copy ..\common\Native\native.js ..\..\core\DesktopEditor\ChromiumBasedEditors2\app\test\src\build\win_64\Release\Local\editors\sdkjs\common\Native\native.js
+
 pause
