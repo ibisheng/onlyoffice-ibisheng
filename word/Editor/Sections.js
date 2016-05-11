512208600c6eab26ee5eaadb55a769508fcbd2ae
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2016-05-11 19:01:18 +0300

    error past cell

--- word/Editor/Sections.js
+++ word/Editor/Sections.js
@@ -25,7 +25,7 @@ function CSectionPr(LogicDocument)
 {
     this.Id = AscCommon.g_oIdCounter.Get_NewId();
 
-    this.Type          = c_oAscSectionBreakType.NextPage;
+    this.Type          = 'undefined' !== typeof c_oAscSectionBreakType ? undefined : c_oAscSectionBreakType.NextPage;
     this.PageSize      = new CSectionPageSize();
     this.PageMargins   = new CSectionPageMargins();
 
