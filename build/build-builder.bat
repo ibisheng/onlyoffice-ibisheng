a6db9cd2786c1750907d7cecde681dd52541b062
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2016-05-13 17:24:23 +0300

    для билдера теперь все также

--- build/build-builder.bat
+++ build/build-builder.bat
@@ -1,8 +1,8 @@
 CD /D %~dp0
 call npm install -g grunt-cli
 call npm install
-rem call grunt --level=WHITESPACE_ONLY --builder=true --formatting=PRETTY_PRINT
-call grunt --level=ADVANCED --builder=true
+rem call grunt --level=WHITESPACE_ONLY --formatting=PRETTY_PRINT
+call grunt --level=ADVANCED
 copy ..\word\sdk-all.js ..\..\core\build\jsbuilder\sdkjs\word\sdk-all.js
 copy ..\word\sdk-all-min.js ..\..\core\build\jsbuilder\sdkjs\word\sdk-all-min.js
 copy ..\slide\sdk-all.js ..\..\core\build\jsbuilder\sdkjs\slide\sdk-all.js
