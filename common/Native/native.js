917d126daf035340f25a503201f5db7fb5f9070e
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2016-05-16 11:19:41 +0300

    config

--- common/Native/native.js
+++ common/Native/native.js
@@ -287,12 +287,12 @@ function NativeOpenFileData(data, version)
 
     if (window.NATIVE_DOCUMENT_TYPE == "presentation" || window.NATIVE_DOCUMENT_TYPE == "document")
     {
-        _api = new window["Asc"]["asc_docs_api"]("");
+        _api = new window["Asc"]["asc_docs_api"]({});
         _api.asc_nativeOpenFile(data, version);
     }
     else
     {
-        _api = new window["Asc"]["spreadsheet_api"]();
+        _api = new window["Asc"]["spreadsheet_api"]({});
         _api.asc_nativeOpenFile(data, version);
     }
 }
