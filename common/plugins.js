8ac92461a56a9e1dd0be2a84f8915ff028b76792
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2016-06-01 14:51:06 +0300

    ocr plugin

--- common/plugins.js
+++ common/plugins.js
@@ -546,6 +546,34 @@ function TEST_PLUGINS()
         			"buttons"         : []
                 }
             ]
+        },
+        {
+            "name" : "ocr(Tesseract.js)",
+            "guid" : "asc.{440EBF13-9B19-4BD8-8621-05200E58140B}",
+            "baseUrl" : "",
+
+            "variations" : [
+                {
+                    "description" : "ocr",
+                    "url"         : "ocr/index.html",
+
+                    "icons"           : ["ocr/icon.png", "ocr/icon@2x.png"],
+                    "isViewer"        : false,
+                    "EditorsSupport"  : ["word"],
+
+                    "isVisual"        : true,
+                    "isModal"         : false,
+                    "isInsideMode"    : false,
+
+                    "initDataType"    : "html",
+                    "initData"        : "",
+
+                    "isUpdateOleOnResize" : false,
+
+                    "buttons"         : [ { "text": "Insert In Document", "primary": true},
+                        { "text": "Cancel", "primary": false } ]
+                }
+            ]
         }
     ];
 
