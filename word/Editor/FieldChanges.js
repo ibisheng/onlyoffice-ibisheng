a600cb36b2d7d88258761cd2a42aa9c5f77e8a31
Ilya Kirillov <Ilya.Kirillov@onlyoffice.com>
2016-11-11 12:26:39 +0300

    Changes for classes ParaField and FootnotesController were replaces with a new classes.

---
+++ word/Editor/FieldChanges.js
@@ -0,0 +1,249 @@
+/*
+ * (c) Copyright Ascensio System SIA 2010-2016
+ *
+ * This program is a free software product. You can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License (AGPL)
+ * version 3 as published by the Free Software Foundation. In accordance with
+ * Section 7(a) of the GNU AGPL its Section 15 shall be amended to the effect
+ * that Ascensio System SIA expressly excludes the warranty of non-infringement
+ * of any third-party rights.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
+ * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+ *
+ * You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia,
+ * EU, LV-1021.
+ *
+ * The  interactive user interfaces in modified source and object code versions
+ * of the Program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU AGPL version 3.
+ *
+ * Pursuant to Section 7(b) of the License you must retain the original Product
+ * logo when distributing the program. Pursuant to Section 7(e) we decline to
+ * grant you any rights under trademark law for use of our trademarks.
+ *
+ * All the Product's GUI elements, including illustrations and icon sets, as
+ * well as technical writing content are licensed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International. See the License
+ * terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+ *
+ */
+
+"use strict";
+/**
+ * User: Ilja.Kirillov
+ * Date: 10.11.2016
+ * Time: 18:59
+ */
+
+AscDFH.changesFactory[AscDFH.historyitem_Field_AddItem]    = CChangesParaFieldAddItem;
+AscDFH.changesFactory[AscDFH.historyitem_Field_RemoveItem] = CChangesParaFieldRemoveItem;
+
+/**
+ * @constructor
+ * @extends {AscDFH.CChangesBase}
+ */
+function CChangesParaFieldAddItem(Class, Pos, Items)
+{
+	CChangesParaFieldAddItem.superclass.constructor.call(this, Class);
+
+	this.Pos    = Pos;
+	this.Items  = Items;
+
+	this.UseArray = false;
+	this.PosArray = [];
+}
+AscCommon.extendClass(CChangesParaFieldAddItem, AscDFH.CChangesBase);
+CChangesParaFieldAddItem.prototype.Type = AscDFH.historyitem_Field_AddItem;
+CChangesParaFieldAddItem.prototype.Undo = function()
+{
+	var oField = this.Class;
+	oField.Content.splice(this.Pos, this.Items.length);
+	oField.protected_UpdateSpellChecking();
+	oField.private_UpdateTrackRevisions();
+};
+CChangesParaFieldAddItem.prototype.Redo = function()
+{
+	var oField = this.Class;
+
+	var Array_start = oField.Content.slice(0, this.Pos);
+	var Array_end   = oField.Content.slice(this.Pos);
+
+	oField.Content = Array_start.concat(this.Items, Array_end);
+	oField.private_UpdateTrackRevisions();
+	oField.protected_UpdateSpellChecking();
+};
+CChangesParaFieldAddItem.prototype.WriteToBinary = function(Writer)
+{
+	// Long     : Количество элементов
+	// Array of :
+	//  {
+	//    Long     : Позиция
+	//    Variable : Id элемента
+	//  }
+
+	var bArray = this.UseArray;
+	var nCount = this.Items.length;
+
+	Writer.WriteLong(nCount);
+	for (var nIndex = 0; nIndex < nCount; ++nIndex)
+	{
+		if (true === bArray)
+			Writer.WriteLong(this.PosArray[nIndex]);
+		else
+			Writer.WriteLong(this.Pos + nIndex);
+
+		Writer.WriteString2(this.Items[nIndex].Get_Id());
+	}
+};
+CChangesParaFieldAddItem.prototype.ReadFromBinary = function(Reader)
+{
+	// Long     : Количество элементов
+	// Array of :
+	//  {
+	//    Long     : Позиция
+	//    Variable : Id Элемента
+	//  }
+
+	this.UseArray = true;
+	this.Items    = [];
+	this.PosArray = [];
+
+	var nCount = Reader.GetLong();
+	for (var nIndex = 0; nIndex < nCount; ++nIndex)
+	{
+		this.PosArray[nIndex] = Reader.GetLong();
+		this.Items[nIndex]    = AscCommon.g_oTableId.Get_ById(Reader.GetString2());
+	}
+};
+CChangesParaFieldAddItem.prototype.Load = function(Color)
+{
+	var oField = this.Class;
+	for (var nIndex = 0, nCount = this.Items.length; nIndex < nCount; ++nIndex)
+	{
+		var Pos     = oField.m_oContentChanges.Check(AscCommon.contentchanges_Add, this.PosArray[nIndex]);
+		var Element = this.Items[nIndex];
+
+		if (null != Element)
+		{
+			oField.Content.splice(Pos, 0, Element);
+			AscCommon.CollaborativeEditing.Update_DocumentPositionsOnAdd(oField, Pos);
+		}
+	}
+
+	oField.private_UpdateTrackRevisions();
+	oField.protected_UpdateSpellChecking();
+};
+/**
+ * @constructor
+ * @extends {AscDFH.CChangesBase}
+ */
+function CChangesParaFieldRemoveItem(Class, Pos, Items)
+{
+	CChangesParaFieldRemoveItem.superclass.constructor.call(this, Class);
+
+	this.Pos    = Pos;
+	this.Items  = Items;
+
+	this.UseArray = false;
+	this.PosArray = [];
+}
+AscCommon.extendClass(CChangesParaFieldRemoveItem, AscDFH.CChangesBase);
+CChangesParaFieldRemoveItem.prototype.Type = AscDFH.historyitem_Field_RemoveItem;
+CChangesParaFieldRemoveItem.prototype.Undo = function()
+{
+	var oField = this.Class;
+
+	var Array_start = oField.Content.slice(0, this.Pos);
+	var Array_end   = oField.Content.slice(this.Pos);
+
+	oField.Content = Array_start.concat(this.Items, Array_end);
+	oField.protected_UpdateSpellChecking();
+	oField.private_UpdateTrackRevisions();
+};
+CChangesParaFieldRemoveItem.prototype.Redo = function()
+{
+	var oField = this.Class;
+	oField.Content.splice(this.Pos, this.Items.length);
+	oField.private_UpdateTrackRevisions();
+	oField.protected_UpdateSpellChecking();
+};
+CChangesParaFieldRemoveItem.prototype.WriteToBinary = function(Writer)
+{
+	// Long          : Количество удаляемых элементов
+	// Array of
+	// {
+	//    Long : позиции удаляемых элементов
+	//    String : id удаляемых элементов
+	// }
+
+	var bArray = this.UseArray;
+	var nCount = this.Items.length;
+
+	var nStartPos = Writer.GetCurPosition();
+	Writer.Skip(4);
+	var nRealCount = nCount;
+
+	for (var nIndex = 0; nIndex < nCount; ++nIndex)
+	{
+		if (true === bArray)
+		{
+			if (false === this.PosArray[nIndex])
+			{
+				nRealCount--;
+			}
+			else
+			{
+				Writer.WriteLong(this.PosArray[nIndex]);
+				Writer.WriteString2(this.Items[nIndex]);
+			}
+		}
+		else
+		{
+			Writer.WriteLong(this.Pos);
+			Writer.WriteString2(this.Items[nIndex]);
+		}
+	}
+
+	var nEndPos = Writer.GetCurPosition();
+	Writer.Seek(nStartPos);
+	Writer.WriteLong(nRealCount);
+	Writer.Seek(nEndPos);
+};
+CChangesParaFieldRemoveItem.prototype.ReadFromBinary = function(Reader)
+{
+	// Long          : Количество удаляемых элементов
+	// Array of
+	// {
+	//    Long : позиции удаляемых элементов
+	//    String : id удаляемых элементов
+	// }
+
+	this.UseArray = true;
+	this.Items    = [];
+	this.PosArray = [];
+
+	var nCount = Reader.GetLong();
+	for (var nIndex = 0; nIndex < nCount; ++nIndex)
+	{
+		this.PosArray[nIndex] = Reader.GetLong();
+		this.Items[nIndex]    = AscCommon.g_oTableId.Get_ById(Reader.GetString2());
+	}
+};
+CChangesParaFieldRemoveItem.prototype.Load = function(Color)
+{
+	var oField = this.Class;
+	for (var nIndex = 0, nCount = this.Items.length; nIndex < nCount; ++nIndex)
+	{
+		var ChangesPos = oField.m_oContentChanges.Check(AscCommon.contentchanges_Remove, this.PosArray[nIndex]);
+
+		if (false === ChangesPos)
+			continue;
+
+		oField.Content.splice(ChangesPos, 1);
+		AscCommon.CollaborativeEditing.Update_DocumentPositionsOnRemove(oField, ChangesPos, 1);
+	}
+	oField.private_UpdateTrackRevisions();
+	oField.protected_UpdateSpellChecking();
+};
