f38c35c78bfa64dcb485e4b1a3c2c59cca5a1e9e
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2016-12-07 14:15:22 +0300

    fix bug 33626 :bug:

--- cell/view/CellTextRender.js
+++ cell/view/CellTextRender.js
@@ -208,14 +208,6 @@
 			}
 
 			co = this.charOffset(pos, i - 1, h);
-
-			// если самый последний символ - это новая строка, то надо сместить еще на одну линию
-			if (t.charWidths[t.chars.length - 1] === 0) {
-				co.left = null;
-				co.top += l[i - 1].th;
-				co.lineIndex++;
-			}
-
 			return co;
 		};
 
@@ -238,10 +230,6 @@
 
 		CellTextRender.prototype.getCharWidth = function (pos) {
 			return this.charWidths[pos];
-		};
-
-		CellTextRender.prototype.isLastCharNL = function () {
-			return this.charWidths[this.chars.length - 1] === 0;
 		};
 
 
