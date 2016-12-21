eca9db3ecd0330763d4a12d6ab4deede76e690f4
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2016-12-21 13:10:17 +0300

    add method GetSelectionRectsBounds

--- cell/view/WorkbookView.js
+++ cell/view/WorkbookView.js
@@ -2999,6 +2999,13 @@
 	WorkbookView.prototype.Is_SelectionUse = function () {
         return !this.getWorksheet().getSelectionShape();
     };
+	WorkbookView.prototype.GetSelectionRectsBounds = function () {
+		var ws = this.getWorksheet();
+		var range = ws.getSelectedRange();
+		var p1 = this.getCellCoord(range.c1, range.r1);
+		var p2 = this.getCellCoord(range.c2, range.r2);
+		return {X : p1._x, Y : p1._y, W : p2._x - p1._x + p2._width., H : p2._y - p1._y + p2._height};
+	};
 
   //------------------------------------------------------------export---------------------------------------------------
   window['AscCommonExcel'] = window['AscCommonExcel'] || {};
