b4391d3b5550bd9733bbc3ce3e2351d8c7459cf1
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2017-01-17 17:49:17 +0300

    change license to 2017

--- word/api.js
+++ word/api.js
@@ -1,5 +1,5 @@
 ﻿/*
- * (c) Copyright Ascensio System SIA 2010-2016
+ * (c) Copyright Ascensio System SIA 2010-2017
  *
  * This program is a free software product. You can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License (AGPL)
