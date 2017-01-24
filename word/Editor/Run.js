aa6430596b7c0697f263a5ced452318130894e98
Ilya Kirillov <Ilya.Kirillov@onlyoffice.com>
2017-01-24 15:15:40 +0300

    Fixed bug #33405. Changing an image wrapping style inside a footnote was forbidden.

--- word/Editor/Run.js
+++ word/Editor/Run.js
@@ -3525,9 +3525,9 @@ ParaRun.prototype.Recalculate_Range_Width = function(PRSC, _CurLine, _CurRange)
                 PRSC.Range.W += Item.Get_Width();
                 PRSC.Range.W += PRSC.SpaceLen;
 
-                // Учитываем только слова и пробелы, идущие после последнего таба
+				// Учитываем только слова и пробелы, идущие после последнего таба
 
-                PRSC.LettersSkip += PRSC.Letters;
+				PRSC.LettersSkip += PRSC.Letters;
                 PRSC.SpacesSkip  += PRSC.Spaces;
 
                 PRSC.Words   = 0;
@@ -5262,7 +5262,7 @@ ParaRun.prototype.Get_ParaContentPosByXY = function(SearchPos, Depth, _CurLine, 
     return Result;
 };
 
-ParaRun.prototype.Get_ParaContentPos = function(bSelection, bStart, ContentPos)
+ParaRun.prototype.Get_ParaContentPos = function(bSelection, bStart, ContentPos, bUseCorrection)
 {
     var Pos = ( true !== bSelection ? this.State.ContentPos : ( false !== bStart ? this.State.Selection.StartPos : this.State.Selection.EndPos ) );
     ContentPos.Add(Pos);
