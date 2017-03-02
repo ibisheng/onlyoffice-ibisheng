7763f43023cd2aa946f0d2a49eee468f739b0f24
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2017-03-02 15:11:34 +0300

    set index for a table at applying changes

--- common/Drawings/Format/GraphicFrame.js
+++ common/Drawings/Format/GraphicFrame.js
@@ -43,7 +43,14 @@ var History = AscCommon.History;
 
 
     window['AscDFH'].drawingsChangesMap[AscDFH.historyitem_GraphicFrameSetSpPr] = function(oClass, value){oClass.spPr = value;};
-    window['AscDFH'].drawingsChangesMap[AscDFH.historyitem_GraphicFrameSetGraphicObject] = function(oClass, value){oClass.graphicObject = value; if(value){value.Parent = oClass;}};
+    window['AscDFH'].drawingsChangesMap[AscDFH.historyitem_GraphicFrameSetGraphicObject] = function(oClass, value){
+        oClass.graphicObject = value;
+        if(value)
+        {
+            value.Parent = oClass;
+            oClass.graphicObject.Index = 0;
+        }
+    };
     window['AscDFH'].drawingsChangesMap[AscDFH.historyitem_GraphicFrameSetSetNvSpPr] = function(oClass, value){oClass.nvGraphicFramePr = value;};
     window['AscDFH'].drawingsChangesMap[AscDFH.historyitem_GraphicFrameSetSetParent] = function(oClass, value){oClass.parent = value;};
     window['AscDFH'].drawingsChangesMap[AscDFH.historyitem_GraphicFrameSetSetGroup] = function(oClass, value){oClass.group = value;};
