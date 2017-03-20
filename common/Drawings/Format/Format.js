a3190ba5d16b3699248260b569e57ff88d64eafd
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2017-03-20 12:55:27 +0300

    CChangesDrawingsBool -> CChangesDrawingsLong

--- common/Drawings/Format/Format.js
+++ common/Drawings/Format/Format.js
@@ -200,7 +200,7 @@ var asc_CShapeProperty = Asc.asc_CShapeProperty;
     AscDFH.changesFactory[AscDFH.historyitem_Xfrm_SetFlipV] = CChangesDrawingsBool;
     AscDFH.changesFactory[AscDFH.historyitem_Xfrm_SetRot] = CChangesDrawingsDouble;
     AscDFH.changesFactory[AscDFH.historyitem_SpPr_SetParent] = CChangesDrawingsObject;
-    AscDFH.changesFactory[AscDFH.historyitem_SpPr_SetBwMode] = CChangesDrawingsBool;
+    AscDFH.changesFactory[AscDFH.historyitem_SpPr_SetBwMode] = CChangesDrawingsLong;
     AscDFH.changesFactory[AscDFH.historyitem_SpPr_SetXfrm] = CChangesDrawingsObject;
     AscDFH.changesFactory[AscDFH.historyitem_SpPr_SetGeometry] = CChangesDrawingsObject;
     AscDFH.changesFactory[AscDFH.historyitem_SpPr_SetFill] = CChangesDrawingsObjectNoId;
@@ -5699,7 +5699,7 @@ CSpPr.prototype =
 
     setBwMode: function(pr)
     {
-        History.Add(new CChangesDrawingsBool(this, AscDFH.historyitem_SpPr_SetBwMode, this.bwMode,  pr));
+        History.Add(new CChangesDrawingsLong(this, AscDFH.historyitem_SpPr_SetBwMode, this.bwMode,  pr));
         this.bwMode = pr;
     },
 
