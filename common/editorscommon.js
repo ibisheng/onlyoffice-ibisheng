269b40de8f06ee092ade021dc3dc8dbc73e48077
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2017-04-13 15:26:49 +0300

    fix jszip load history

--- common/editorscommon.js
+++ common/editorscommon.js
@@ -254,98 +254,104 @@ function loadFileContent(url, callback) {
   });
 }
 
-function getJSZipUtils() {
-  return window['JSZipUtils'] ? window['JSZipUtils'] : require('jsziputils');
-}
-function getImageFromChanges (name) {
-	var file;
-	var ext = GetFileExtension(name);
-	if (null !== ext && oZipChanges && (file = oZipChanges.files[name])) {
-		var oFileArray = file.asUint8Array();
-		return 'data:image/' + ext + ';base64,' + AscCommon.Base64Encode(oFileArray, oFileArray.length, 0);
+	function getJSZipUtils() {
+		return window['JSZipUtils'] ? window['JSZipUtils'] : require('jsziputils');
 	}
-	return null;
-}
-function openFileCommand(binUrl, changesUrl, Signature, callback) {
-  var bError = false, oResult = new OpenFileResult(), bEndLoadFile = false, bEndLoadChanges = false;
-  var onEndOpen = function() {
-    if (bEndLoadFile && bEndLoadChanges) {
-      if (callback) {
-        callback(bError, oResult);
-      }
-    }
-  };
-  var sFileUrl = binUrl;
-  sFileUrl = sFileUrl.replace(/\\/g, "/");
 
-  if (!window['IS_NATIVE_EDITOR']) {
-    asc_ajax({
-    url: sFileUrl,
-    dataType: "text",
-    success: function(result) {
-      //получаем url к папке с файлом
-      var url;
-      var nIndex = sFileUrl.lastIndexOf("/");
-      url = (-1 !== nIndex) ? sFileUrl.substring(0, nIndex + 1) : sFileUrl;
-      if (0 < result.length) {
-        oResult.bSerFormat = Signature === result.substring(0, Signature.length);
-        oResult.data = result;
-        oResult.url = url;
-      } else {
-        bError = true;
-      }
-      bEndLoadFile = true;
-      onEndOpen();
-    },
-    error: function() {
-      bEndLoadFile = true;
-      bError = true;
-      onEndOpen();
-    }
-  });
-  }
+	function getImageFromChanges(name) {
+		var file;
+		var ext = GetFileExtension(name);
+		if (null !== ext && oZipChanges && (file = oZipChanges.files[name])) {
+			var oFileArray = file.asUint8Array();
+			return 'data:image/' + ext + ';base64,' + AscCommon.Base64Encode(oFileArray, oFileArray.length, 0);
+		}
+		return null;
+	}
 
-  if (null != changesUrl) {
-    getJSZipUtils().getBinaryContent(changesUrl, function(err, data) {
-      bEndLoadChanges = true;
-      if (err) {
-        bError = true;
-        onEndOpen();
-        return;
-      }
+	function openFileCommand(binUrl, changesUrl, Signature, callback) {
+		var bError = false, oResult = new OpenFileResult(), bEndLoadFile = false, bEndLoadChanges = false;
+		var onEndOpen = function () {
+			if (bEndLoadFile && bEndLoadChanges) {
+				if (callback) {
+					callback(bError, oResult);
+				}
+			}
+		};
+		var sFileUrl = binUrl;
+		sFileUrl = sFileUrl.replace(/\\/g, "/");
 
-      oZipChanges = new (require('jszip'))(data);
-      oResult.changes = [];
-      for (var i in oZipChanges.files) {
-        if (i.endsWith('.json')) {
-          // Заглушка на имя файла (стоило его начинать с цифры)
-          oResult.changes[parseInt(i.slice('changes'.length))] = JSON.parse(oZipChanges.files[i].asText());
-        }
-      }
-      onEndOpen();
-    });
-  } else {
-    bEndLoadChanges = true;
-  }
-
-	if (window['IS_NATIVE_EDITOR']) {
-		var result = window["native"]["openFileCommand"](sFileUrl, changesUrl, Signature);
-
-		var url;
-		var nIndex = sFileUrl.lastIndexOf("/");
-		url = (-1 !== nIndex) ? sFileUrl.substring(0, nIndex + 1) : sFileUrl;
-		if (0 < result.length) {
-			oResult.bSerFormat = Signature === result.substring(0, Signature.length);
-			oResult.data = result;
-			oResult.url = url;
-		} else {
-			bError = true;
+		if (!window['IS_NATIVE_EDITOR']) {
+			asc_ajax({
+				url: sFileUrl, dataType: "text", success: function (result) {
+					//получаем url к папке с файлом
+					var url;
+					var nIndex = sFileUrl.lastIndexOf("/");
+					url = (-1 !== nIndex) ? sFileUrl.substring(0, nIndex + 1) : sFileUrl;
+					if (0 < result.length) {
+						oResult.bSerFormat = Signature === result.substring(0, Signature.length);
+						oResult.data = result;
+						oResult.url = url;
+					} else {
+						bError = true;
+					}
+					bEndLoadFile = true;
+					onEndOpen();
+				}, error: function () {
+					bEndLoadFile = true;
+					bError = true;
+					onEndOpen();
+				}
+			});
 		}
 
-		bEndLoadFile = true;
-		onEndOpen();
+		if (changesUrl) {
+			getJSZipUtils().getBinaryContent(changesUrl, function (err, data) {
+				if (err) {
+					bEndLoadChanges = true;
+					bError = true;
+					onEndOpen();
+					return;
+				}
+
+				oResult.changes = [];
+				require('jszip').loadAsync(data).then(function (zipChanges) {
+					var promises = [];
+					zipChanges.forEach(function (relativePath, file) {
+						if (relativePath.endsWith('.json')) {
+							promises[parseInt(relativePath.slice('changes'.length))] = file.async('string');
+						}
+					});
+					Promise.all(promises).then(function (values) {
+						for (var i = 0; i < values.length; ++i) {
+							oResult.changes[i] = JSON.parse(values[i]);
+						}
+						bEndLoadChanges = true;
+						onEndOpen();
+					});
+				});
+			});
+		} else {
+			bEndLoadChanges = true;
+		}
+
+		if (window['IS_NATIVE_EDITOR']) {
+			var result = window["native"]["openFileCommand"](sFileUrl, changesUrl, Signature);
+
+			var url;
+			var nIndex = sFileUrl.lastIndexOf("/");
+			url = (-1 !== nIndex) ? sFileUrl.substring(0, nIndex + 1) : sFileUrl;
+			if (0 < result.length) {
+				oResult.bSerFormat = Signature === result.substring(0, Signature.length);
+				oResult.data = result;
+				oResult.url = url;
+			} else {
+				bError = true;
+			}
+
+			bEndLoadFile = true;
+			onEndOpen();
+		}
 	}
- }
  function sendCommand(editor, fCallback, rdata, dataContainer) {
   //json не должен превышать размера 2097152, иначе при его чтении будет exception
   var docConnectionId = editor.CoAuthoringApi.getDocId();
