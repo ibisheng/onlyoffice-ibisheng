069febbb26452a3589eac82fda6a7139ad7686c0
GoshaZotov <Igor.Zotov@onlyoffice.com>
2017-05-30 16:14:13 +0300

    fix bug 35025

--- cell/model/FormulaObjects/mathematicFunctions.js
+++ cell/model/FormulaObjects/mathematicFunctions.js
@@ -321,7 +321,7 @@
 				return NaN;
 			}
 
-			var chars = {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1};
+			var chars = {"M":1000, "CM":900, "D":500, "CD":400, "C":100, "XC":90, "L":50, "XL":40, "X":10, "IX":9, "V":5, "IV":4, "I":1};
 			var arabic = 0;
 			roman.replace(/[MDLV]|C[MD]?|X[CL]?|I[XV]?/g, function(i){
 				arabic += chars[i];
