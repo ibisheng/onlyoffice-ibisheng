72ac4dad13b789ba776296dad328907f9b251589
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2017-06-28 14:44:51 +0300

    add version variables

--- build/build-builder.bat
+++ build/build-builder.bat
@@ -1,8 +1,13 @@
 CD /D %~dp0
 call npm install -g grunt-cli
 call npm install
+
+SET PRODUCT_VERSION=4.4.1
+SET BUILD_NUMBER=1
+
 rem call grunt --level=WHITESPACE_ONLY --formatting=PRETTY_PRINT
 call grunt --level=ADVANCED
+
 copy ..\word\sdk-all.js ..\..\core\build\jsbuilder\sdkjs\word\sdk-all.js
 copy ..\word\sdk-all-min.js ..\..\core\build\jsbuilder\sdkjs\word\sdk-all-min.js
 copy ..\slide\sdk-all.js ..\..\core\build\jsbuilder\sdkjs\slide\sdk-all.js
