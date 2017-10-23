2adb9ac217a1db21327d3af4e5873fa73076f93a
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2017-10-23 15:32:09 +0300

    delete unused argument

--- cell/model/Workbook.js
+++ cell/model/Workbook.js
@@ -2432,7 +2432,7 @@
 			this.aWorksheets[i].updateSparklineCache(sheet, ranges);
 		}
 	};
-	Workbook.prototype.sortDependency = function (setCellFormat) {
+	Workbook.prototype.sortDependency = function () {
 		this.dependencyFormulas.calcTree();
 	};
 	/**
