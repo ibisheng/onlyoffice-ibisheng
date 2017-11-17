1baea43d49a36ab43950e3c689d09b30b9fcb017
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2017-11-17 18:37:27 +0300

    TablePart -> TableParts

--- cell/model/autofilters.js
+++ cell/model/autofilters.js
@@ -2793,7 +2793,7 @@
 
 				if(worksheet.TableParts && worksheet.TableParts.length) {
 					for (var i = 0; i < worksheet.TableParts.length; i++) {
-						var tablePart = worksheet.TablePart[i];
+						var tablePart = worksheet.TableParts[i];
 						if(tablePart.isHeaderRow()){
 							var headerRange = new Asc.Range(tablePart.Ref.c1, tablePart.Ref.r1, tablePart.Ref.c2, tablePart.Ref.r1);
 							if(headerRange.contains(col, row)){
