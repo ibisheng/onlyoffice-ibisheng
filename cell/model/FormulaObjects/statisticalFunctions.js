d55b84a8d304b6066744c780833bfd1f45b10b3f
GoshaZotov <Igor.Zotov@onlyoffice.com>
2017-11-22 16:37:46 +0300

    fix bug 36414

--- cell/model/FormulaObjects/statisticalFunctions.js
+++ cell/model/FormulaObjects/statisticalFunctions.js
@@ -2701,7 +2701,7 @@ function (window, undefined) {
 
 			if (nColMatX !== nColMatY || nRowMatX !== nRowMatY && !checkNumericMatrix(rMatX) ||
 				!checkNumericMatrix(rMatY)) {
-				return new cError(cErrorType.wrong_value_type);
+				return new cError(cErrorType.not_available);
 			}
 
 			this.bEDS = ( rSmplInPrd === 0 );
