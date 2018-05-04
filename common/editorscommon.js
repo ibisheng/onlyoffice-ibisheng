45bac5f27b109223f6f9a7ba0751e8f0b657e342
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2018-05-04 19:34:00 +0300

    [de][se][pe] Add savefile request handler

--- common/editorscommon.js
+++ common/editorscommon.js
@@ -141,6 +141,7 @@
 	var sDownloadServiceLocalUrl = "../../../../downloadas";
 	var sUploadServiceLocalUrl = "../../../../upload";
 	var sUploadServiceLocalUrlOld = "../../../../uploadold";
+	var sSaveFileLocalUrl = "../../../../savefile";
 	var nMaxRequestLength = 5242880;//5mb <requestLimits maxAllowedContentLength="30000000" /> default 30mb
 
 	function getSockJs()
@@ -685,6 +686,19 @@
 								 fCallback(JSON.parse(httpRequest.responseText), true);
 							 }
 						 }
+		});
+	}
+
+	function sendSaveFile(docId, userId, title, jwt, data, fError, fsuccess)
+	{
+		var cmd = {'id': docId, "userid": userId, "jwt": jwt, 'outputpath': title};
+		asc_ajax({
+			type:        'POST',
+			url:         sSaveFileLocalUrl + '/' + docId + '?cmd=' + encodeURIComponent(JSON.stringify(cmd)),
+			data:        data,
+			contentType: "application/octet-stream",
+			error:       fError,
+			success:     fsuccess
 		});
 	}
 
@@ -3420,6 +3434,7 @@
 	window["AscCommon"].getImageFromChanges = getImageFromChanges;
 	window["AscCommon"].openFileCommand = openFileCommand;
 	window["AscCommon"].sendCommand = sendCommand;
+	window["AscCommon"].sendSaveFile = sendSaveFile;
 	window["AscCommon"].mapAscServerErrorToAscError = mapAscServerErrorToAscError;
 	window["AscCommon"].joinUrls = joinUrls;
 	window["AscCommon"].getFullImageSrc2 = getFullImageSrc2;
