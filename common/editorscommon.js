6bfd42cb690d4c877f340a3f390200c066b11a90
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2018-06-09 17:07:44 +0300

    Password on crypto mode (own file password)

--- common/editorscommon.js
+++ common/editorscommon.js
@@ -3945,12 +3945,14 @@ window["asc_initAdvancedOptions"] = function(_code, _file_hash)
 {
     var _editor = window["Asc"]["editor"] ? window["Asc"]["editor"] : window.editor;
 
-    if ((_code == 90 || _code == 91) && window.g_asc_plugins && window.g_asc_plugins.isRunnedEncryption())
+    if ((_code == 90 || _code == 91) && window.g_asc_plugins && window.g_asc_plugins.isRunnedEncryption() && !window.checkPasswordFromPlugin)
     {
+    	window.checkPasswordFromPlugin = true;
         window.g_asc_plugins.sendToEncryption({ "type" : "getPasswordByFile", "hash" : _file_hash });
         return;
     }
 
+    window.checkPasswordFromPlugin = false;
     _editor._onNeedParams(undefined, (_code == 90 || _code == 91) ? true : undefined);
 };
 window.openFileCryptCallback = function(_binary)
