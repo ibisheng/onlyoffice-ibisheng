0643926d3ea0e38ea9a6eb53add44a0501f1fdaf
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2018-07-12 20:49:51 +0300

    [pe][mobile] Send slide properties

--- slide/Native/Wrappers/native.js
+++ slide/Native/Wrappers/native.js
@@ -93,8 +93,8 @@ Asc['asc_docs_api'].prototype.sync_EndCatchSelectedElements = function()
             case Asc.c_oAscTypeSelectElement.Slide:
             {
                 console.log("StackObjects -> Slide");
-                // _stream["WriteLong"](Asc.c_oAscTypeSelectElement.Slide);
-                // asc_menu_WriteSlidePr(this.SelectedObjectsStack[i].Value, _stream);
+                _stream["WriteLong"](Asc.c_oAscTypeSelectElement.Slide);
+                asc_menu_WriteSlidePr(this.SelectedObjectsStack[i].Value, _stream);
                 break;
             }
           
