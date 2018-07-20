eafa268027354b8607e749f919c30f91e9522f1a
GoshaZotov <Igor.Zotov@onlyoffice.com>
2018-07-20 11:19:38 +0300

    [se] TurnOff history if read from binary

--- cell/model/clipboard.js
+++ cell/model/clipboard.js
@@ -1187,13 +1187,15 @@
 				var tempWorkbook = new AscCommonExcel.Workbook();
 				var t = this;
 				var newFonts;
-				
+
+				History.TurnOff();
 				pptx_content_loader.Start_UseFullUrl();
-                pptx_content_loader.Reader.ClearConnectorsMaps();
+				pptx_content_loader.Reader.ClearConnectorsMaps();
 				oBinaryFileReader.Read(base64, tempWorkbook);
 				this.activeRange = oBinaryFileReader.copyPasteObj.activeRange;
 				var aPastedImages = pptx_content_loader.End_UseFullUrl();
-                pptx_content_loader.Reader.AssignConnectorsId();
+				pptx_content_loader.Reader.AssignConnectorsId();
+				History.TurnOn();
 				
 				var pasteData = null;
 				if (tempWorkbook)
